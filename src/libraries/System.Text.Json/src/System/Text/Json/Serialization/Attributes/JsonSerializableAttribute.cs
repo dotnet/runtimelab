1c6fc684a2d1fd6bd07a9d208b799f99eebe85f3
Kyungtak Woo <kevinwkt1997@gmail.com>
2020-07-08 01:25:19 -0500

    Create JsonSerializableAttribute attribute for type discovery

---
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Attributes/JsonSerializableAttribute.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Text.Json.Serialization
+{
+    /// <summary>
+    /// When placed on a type, will source generate de/serialization for the specified type and it's descendants.
+    /// </summary>
+    /// <remarks>
+    /// Must take into account that type discovery using this attribute is at compile time using Source Generators.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
+    public sealed class JsonSerializableAttribute : JsonAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="JsonSerializableAttribute"/>.
+        /// </summary>
+        public JsonSerializableAttribute() { }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="JsonSerializableAttribute"/> with the specified type.
+        /// </summary>
+        /// <param name="type">The Type of the property.</param>
+        public JsonSerializableAttribute(Type type) { }
+    }
+}
