a184a8f95a931f49e129ac79469fe0a673a45bb0
Kyungtak Woo <kevinwkt1997@gmail.com>
2020-08-17 21:55:36 -0500

    Added more collections relying on T

--- src/libraries/System.Text.Json/src/System/Text/Json/Serialization/JsonSerializerContext.Known.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/JsonSerializerContext.Known.cs
@@ -76,6 +76,38 @@ namespace System.Text.Json.Serialization
             }
         }
 
+        private JsonTypeInfo<DateTime>? _dateTime;
+        private static JsonTypeInfo<DateTime>? s_dateTime;
+        /// <summary>
+        /// todo
+        /// </summary>
+        public JsonTypeInfo<DateTime> DateTime
+        {
+            get
+            {
+                if (_dateTime == null)
+                {
+                    // todo: support obtaining existing converter
+                    //if (_options.HasCustomConverters)
+                    //{
+                    //JsonConverter<DateTime> converter = (JsonConverter<DateTime>)_options.GetConverter(typeof(DateTime));
+                    //_dateTime = new JsonValueInfo<DateTime>(converter, _options);
+                    //}
+                    //else
+                    {
+                        if (s_dateTime == null)
+                        {
+                            s_dateTime = new JsonValueInfo<DateTime>(new DateTimeConverter(), _options);
+                        }
+
+                        _dateTime = s_dateTime;
+                    }
+                }
+
+                return _dateTime;
+            }
+        }
+
         private JsonTypeInfo<int>? _int32;
         private static JsonTypeInfo<int>? s_int32;
         /// <summary>
@@ -105,6 +137,134 @@ namespace System.Text.Json.Serialization
                 }
 
                 return _int32;
+            }
+        }
+
+        private JsonTypeInfo<long>? _int64;
+        private static JsonTypeInfo<long>? s_int64;
+        /// <summary>
+        /// todo
+        /// </summary>
+        public JsonTypeInfo<long> Int64
+        {
+            get
+            {
+                if (_int64 == null)
+                {
+                    // todo: support obtaining existing converter
+                    //if (_options.HasCustomConverters)
+                    //{
+                    //JsonConverter<long> converter = (JsonConverter<long>)_options.GetConverter(typeof(long));
+                    //_int64 = new JsonValueInfo<long>(converter, _options);
+                    //}
+                    //else
+                    {
+                        if (s_int64 == null)
+                        {
+                            s_int64 = new JsonValueInfo<long>(new Int64Converter(), _options);
+                        }
+
+                        _int64 = s_int64;
+                    }
+                }
+
+                return _int64;
+            }
+        }
+
+        private JsonTypeInfo<float>? _single;
+        private static JsonTypeInfo<float>? s_single;
+        /// <summary>
+        /// todo
+        /// </summary>
+        public JsonTypeInfo<float> Single
+        {
+            get
+            {
+                if (_single == null)
+                {
+                    // todo: support obtaining existing converter
+                    //if (_options.HasCustomConverters)
+                    //{
+                    //JsonConverter<float> converter = (JsonConverter<float>)_options.GetConverter(typeof(float));
+                    //_single = new JsonValueInfo<float>(converter, _options);
+                    //}
+                    //else
+                    {
+                        if (s_single == null)
+                        {
+                            s_single = new JsonValueInfo<float>(new SingleConverter(), _options);
+                        }
+
+                        _single = s_single;
+                    }
+                }
+
+                return _single;
+            }
+        }
+
+        private JsonTypeInfo<double>? _double;
+        private static JsonTypeInfo<double>? s_double;
+        /// <summary>
+        /// todo
+        /// </summary>
+        public JsonTypeInfo<double> Double
+        {
+            get
+            {
+                if (_double == null)
+                {
+                    // todo: support obtaining existing converter
+                    //if (_options.HasCustomConverters)
+                    //{
+                    //JsonConverter<double> converter = (JsonConverter<double>)_options.GetConverter(typeof(double));
+                    //_double = new JsonValueInfo<double>(converter, _options);
+                    //}
+                    //else
+                    {
+                        if (s_double == null)
+                        {
+                            s_double = new JsonValueInfo<double>(new DoubleConverter(), _options);
+                        }
+
+                        _double = s_double;
+                    }
+                }
+
+                return _double;
+            }
+        }
+
+        private JsonTypeInfo<char>? _char;
+        private static JsonTypeInfo<char>? s_char;
+        /// <summary>
+        /// todo
+        /// </summary>
+        public JsonTypeInfo<char> Char
+        {
+            get
+            {
+                if (_char == null)
+                {
+                    // todo: support obtaining existing converter
+                    //if (_options.HasCustomConverters)
+                    //{
+                    //JsonConverter<char> converter = (JsonConverter<char>)_options.GetConverter(typeof(char));
+                    //_char = new JsonValueInfo<char>(converter, _options);
+                    //}
+                    //else
+                    {
+                        if (s_char == null)
+                        {
+                            s_char = new JsonValueInfo<char>(new CharConverter(), _options);
+                        }
+
+                        _char = s_char;
+                    }
+                }
+
+                return _char;
             }
         }
 
--- src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Metadata/KnownCollectionTypeInfos.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Metadata/KnownCollectionTypeInfos.cs
@@ -18,6 +18,27 @@ namespace System.Text.Json.Serialization.Metadata
     /// <typeparam name="T"></typeparam>
     public static class KnownCollectionTypeInfos<T>
     {
+        private static JsonTypeInfo<T[]>? s_array;
+        /// <summary>
+        /// todo
+        /// </summary>
+        public static JsonTypeInfo<T[]> GetArray(JsonClassInfo elementInfo, JsonSerializerContext context)
+        {
+            // todo: support obtaining existing converter
+            //if (context._options.HasCustomConverters)
+            //{
+            //JsonConverter<T[]> converter = (JsonConverter<T[]>)context._options.GetConverter(typeof(T[]));
+            //return new JsonCollectionTypeInfo<T[]>(CreateList, converter, elementInfo, context._options);
+            //}
+
+            if (s_array == null)
+            {
+                s_array = new JsonCollectionTypeInfo<T[]>(CreateList, new ArrayConverter<T[], T>(), elementInfo, context._options);
+            }
+
+            return s_array;
+        }
+
         private static JsonTypeInfo<List<T>>? s_list;
         /// <summary>
         /// todo
@@ -39,9 +60,82 @@ namespace System.Text.Json.Serialization.Metadata
             return s_list;
         }
 
+        private static JsonTypeInfo<IEnumerable<T>>? s_ienumerable;
+        /// <summary>
+        /// todo
+        /// </summary>
+        public static JsonTypeInfo<IEnumerable<T>> GetIEnumerable(JsonClassInfo elementInfo, JsonSerializerContext context)
+        {
+            // todo: support obtaining existing converter
+            //if (context._options.HasCustomConverters)
+            //{
+            //JsonConverter<IEnumerable<T>> converter = (JsonConverter<IEnumerable<T>>)context._options.GetConverter(typeof(IEnumerable<T>));
+            //return new JsonCollectionTypeInfo<IEnumerable<T>>(CreateList, converter, elementInfo, context._options);
+            //}
+
+            if (s_ienumerable == null)
+            {
+                s_ienumerable = new JsonCollectionTypeInfo<IEnumerable<T>>(CreateList, new IEnumerableOfTConverter<IEnumerable<T>, T>(), elementInfo, context._options);
+            }
+
+            return s_ienumerable;
+        }
+
+        private static JsonTypeInfo<Queue<T>>? s_queue;
+        /// <summary>
+        /// todo
+        /// </summary>
+        public static JsonTypeInfo<Queue<T>> GetQueue(JsonClassInfo elementInfo, JsonSerializerContext context)
+        {
+            // todo: support obtaining existing converter
+            //if (context._options.HasCustomConverters)
+            //{
+            //JsonConverter<Queue<T>> converter = (JsonConverter<Queue<T>>)context._options.GetConverter(typeof(Queue<T>));
+            //return new JsonCollectionTypeInfo<Queue<T>>(CreateQueue, converter, elementInfo, context._options);
+            //}
+
+            if (s_queue == null)
+            {
+                s_queue = new JsonCollectionTypeInfo<Queue<T>>(CreateQueue, new QueueOfTConverter<Queue<T>, T>(), elementInfo, context._options);
+            }
+
+            return s_queue;
+        }
+
+        private static JsonTypeInfo<Stack<T>>? s_stack;
+        /// <summary>
+        /// todo
+        /// </summary>
+        public static JsonTypeInfo<Stack<T>> GetStack(JsonClassInfo elementInfo, JsonSerializerContext context)
+        {
+            // todo: support obtaining existing converter
+            //if (context._options.HasCustomConverters)
+            //{
+            //JsonConverter<Stack<T>> converter = (JsonConverter<Stack<T>>)context._options.GetConverter(typeof(Stack<T>));
+            //return new JsonCollectionTypeInfo<Stack<T>>(CreateStack, converter, elementInfo, context._options);
+            //}
+
+            if (s_stack == null)
+            {
+                s_stack = new JsonCollectionTypeInfo<Stack<T>>(CreateStack, new StackOfTConverter<Stack<T>, T>(), elementInfo, context._options);
+            }
+
+            return s_stack;
+        }
+
         private static List<T> CreateList()
         {
             return new List<T>();
+        }
+
+        private static Queue<T> CreateQueue()
+        {
+            return new Queue<T>();
+        }
+
+        private static Stack<T> CreateStack()
+        {
+            return new Stack<T>();
         }
 
         // todo: duplicate the above code for each supported collection type (IEnumerable, IEnumerable<T>, array, etc)
