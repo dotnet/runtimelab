6bc40c43492fbb49af196e534deefd0066fb2901
Elinor Fung <elfung@microsoft.com>
2020-09-05 11:39:17 -0700

    Add test cases

--- DllImportGenerator/DllImportGenerator.Test/Compiles.cs
+++ DllImportGenerator/DllImportGenerator.Test/Compiles.cs
@@ -1,5 +1,7 @@
 using Microsoft.CodeAnalysis;
+using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -16,10 +18,43 @@ namespace DllImportGenerator.Test
             yield return new[] { CodeSnippets.NestedTypes };
             yield return new[] { CodeSnippets.UserDefinedEntryPoint };
             yield return new[] { CodeSnippets.AllDllImportNamedArguments };
-            yield return new[] { CodeSnippets.BasicParametersAndModifiers };
             yield return new[] { CodeSnippets.DefaultParameters };
             yield return new[] { CodeSnippets.UseCSharpFeaturesForConstants };
             yield return new[] { CodeSnippets.MarshalAsAttributeOnTypes };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<byte>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<sbyte>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<short>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<ushort>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<int>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<uint>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<long>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<ulong>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<float>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<double>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<bool>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<char>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<string>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<IntPtr>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<UIntPtr>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<byte[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<sbyte[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<short[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<ushort[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<int[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<uint[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<long[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<ulong[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<float[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<double[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<bool[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<char[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<string[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<IntPtr[]>() };
+            yield return new[] { CodeSnippets.BasicParametersAndModifiers<UIntPtr[]>() };
+            yield return new[] { CodeSnippets.MarshalAsParametersAndModifiers<bool>(UnmanagedType.Bool) };
+            yield return new[] { CodeSnippets.MarshalAsParametersAndModifiers<bool>(UnmanagedType.VariantBool) };
+            yield return new[] { CodeSnippets.MarshalAsParametersAndModifiers<bool>(UnmanagedType.I1) };
+            yield return new[] { CodeSnippets.EnumParameters };
         }
 
         [Theory]
