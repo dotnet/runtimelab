5c245f93a2fe898b2313f6cf9faf2a7b47ab365b
Jan Kotas <jkotas@microsoft.com>
2020-09-23 21:32:46 -0700

    Fix build breaks

--- src/coreclr/src/nativeaot/System.Private.CoreLib/src/System/Threading/Monitor.CoreRT.cs
+++ src/coreclr/src/nativeaot/System.Private.CoreLib/src/System/Threading/Monitor.CoreRT.cs
@@ -20,7 +20,7 @@ using Internal.Runtime.CompilerServices;
 
 namespace System.Threading
 {
-    public static class Monitor
+    public static partial class Monitor
     {
         #region Object->Lock/Condition mapping
 
@@ -121,12 +121,6 @@ namespace System.Threading
             lockTaken = TryAcquireContended(lck, obj, millisecondsTimeout);
         }
 
-        public static bool TryEnter(object obj, TimeSpan timeout) =>
-            TryEnter(obj, WaitHandle.ToTimeoutMilliseconds(timeout));
-
-        public static void TryEnter(object obj, TimeSpan timeout, ref bool lockTaken) =>
-            TryEnter(obj, WaitHandle.ToTimeoutMilliseconds(timeout), ref lockTaken);
-
         public static void Exit(object obj)
         {
             GetLock(obj).Release();
@@ -152,22 +146,6 @@ namespace System.Threading
                 return condition.Wait(millisecondsTimeout);
             }
         }
-
-        [UnsupportedOSPlatform("browser")]
-        public static bool Wait(object obj, TimeSpan timeout) => Wait(obj, WaitHandle.ToTimeoutMilliseconds(timeout));
-
-        [UnsupportedOSPlatform("browser")]
-        public static bool Wait(object obj) => Wait(obj, Timeout.Infinite);
-
-        // Remoting is not supported, exitContext argument is unused
-        [UnsupportedOSPlatform("browser")]
-        public static bool Wait(object obj, int millisecondsTimeout, bool exitContext) =>
-            Wait(obj, millisecondsTimeout);
-
-        // Remoting is not supported, exitContext argument is unused
-        [UnsupportedOSPlatform("browser")]
-        public static bool Wait(object obj, TimeSpan timeout, bool exitContext) =>
-            Wait(obj, WaitHandle.ToTimeoutMilliseconds(timeout));
 
         public static void Pulse(object obj)
         {
