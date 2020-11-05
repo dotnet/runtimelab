12bc93648b32d6181c72aeb4c543d313804792ea
Cory Nelson <phrosty@gmail.com>
2020-11-05 13:19:04 -0800

    Add dependencies installer.

---
+++ src/install-dependencies.ps1
@@ -0,0 +1,32 @@
+Set-StrictMode -Version 'Latest'
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
+
+# Root directory of the project.
+$RootDir = Split-Path $PSScriptRoot -Parent
+$NuGetPath = Join-Path $RootDir "nuget"
+
+# Well-known location for clog packages.
+$ClogDownloadUrl = "https://github.com/microsoft/CLOG/releases/download/v0.1.2"
+$ClogVersion = "0.1.2"
+
+function Install-ClogTool {
+    param($ToolName)
+    New-Item -Path $NuGetPath -ItemType Directory -Force | Out-Null
+    $NuGetName = "$ToolName.$ClogVersion.nupkg"
+    $NuGetFile = Join-Path $NuGetPath $NuGetName
+    try {
+        if (!(Test-Path $NuGetFile)) {
+            Write-Host "Downloading $NuGetName"
+            Invoke-WebRequest -Uri "$ClogDownloadUrl/$NuGetName" -OutFile $NuGetFile
+        }
+        Write-Host "Installing: $NuGetName"
+        dotnet tool update --local --add-source $NuGetPath $ToolName
+    } catch {
+        Write-Warning "Clog could not be installed. Building with logs will not work"
+        Write-Warning $_
+    }
+}
+
+Install-ClogTool "Microsoft.Logging.CLOG"
+Install-ClogTool "Microsoft.Logging.CLOG2Text.Windows"
