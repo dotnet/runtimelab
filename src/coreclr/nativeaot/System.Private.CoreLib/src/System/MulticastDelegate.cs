29a733eba5539d5e3c34d8330d46b17d5f1fb96a
Jan Kotas <jkotas@microsoft.com>
2020-12-15 22:58:51 -0800

    Fix to match contacts

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/MulticastDelegate.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/MulticastDelegate.cs
@@ -186,21 +186,21 @@ namespace System
 
         // This method will combine this delegate with the passed delegate
         //  to form a new delegate.
-        protected override Delegate CombineImpl(Delegate d)
+        protected override Delegate CombineImpl(Delegate? follow)
         {
-            if (d is null) // cast to object for a more efficient test
+            if (follow is null) // cast to object for a more efficient test
                 return this;
 
             // Verify that the types are the same...
-            if (!InternalEqualTypes(this, d))
+            if (!InternalEqualTypes(this, follow))
                 throw new ArgumentException(SR.Arg_DlgtTypeMis);
 
-            if (IsDynamicDelegate() && d.IsDynamicDelegate())
+            if (IsDynamicDelegate() && follow.IsDynamicDelegate())
             {
                 throw new InvalidOperationException();
             }
 
-            MulticastDelegate dFollow = (MulticastDelegate)d;
+            MulticastDelegate dFollow = (MulticastDelegate)follow;
             Delegate[] resultList;
             int followCount = 1;
             Delegate[] followList = dFollow.m_helperObject as Delegate[];
@@ -309,12 +309,12 @@ namespace System
         //  look at the invocation list.)  If this is found we remove it from
         //  this list and return a new delegate.  If its not found a copy of the
         //  current list is returned.
-        protected override Delegate RemoveImpl(Delegate d)
+        protected override Delegate? RemoveImpl(Delegate value)
         {
             // There is a special case were we are removing using a delegate as
             //    the value we need to check for this case
             //
-            MulticastDelegate v = d as MulticastDelegate;
+            MulticastDelegate v = value as MulticastDelegate;
 
             if (v is null)
                 return this;
@@ -324,7 +324,7 @@ namespace System
                 if (invocationList == null)
                 {
                     // they are both not real Multicast
-                    if (this.Equals(d))
+                    if (this.Equals(v))
                         return null;
                 }
                 else
@@ -332,7 +332,7 @@ namespace System
                     int invocationCount = (int)m_extraFunctionPointerOrData;
                     for (int i = invocationCount; --i >= 0;)
                     {
-                        if (d.Equals(invocationList[i]))
+                        if (v.Equals(invocationList[i]))
                         {
                             if (invocationCount == 2)
                             {
