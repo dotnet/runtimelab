d995a22bd3f292fd5ac70934fe29bd20291ecb89
Jan Kotas <jkotas@microsoft.com>
2020-12-16 01:21:36 -0800

    Eliminate CORERT ifdef (#470)

--- src/libraries/System.Private.CoreLib/src/System/Delegate.cs
+++ src/libraries/System.Private.CoreLib/src/System/Delegate.cs
@@ -52,7 +52,6 @@ namespace System
         [RequiresUnreferencedCode("The target method might be removed")]
         public static Delegate CreateDelegate(Type type, Type target, string method, bool ignoreCase) => CreateDelegate(type, target, method, ignoreCase, throwOnBindFailure: true)!;
 
-#if !CORERT
         protected virtual Delegate CombineImpl(Delegate? d) => throw new MulticastNotSupportedException(SR.Multicast_Combine);
 
         protected virtual Delegate? RemoveImpl(Delegate d) => d.Equals(this) ? null : this;
@@ -63,7 +62,6 @@ namespace System
         {
             return DynamicInvokeImpl(args);
         }
-#endif
 
         public virtual void GetObjectData(SerializationInfo info, StreamingContext context) => throw new PlatformNotSupportedException();
 
