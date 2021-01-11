54321e38193b175b4b62b3e2399d4c50a22af5a6
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-01-11 14:11:45 -0800

    Add Benchmarks project. (#538)

---
+++ DllImportGenerator/Benchmarks/BlittableTypes.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    partial class NativeExportsNE
+    {
+        public const string NativeExportsNE_Binary = "Microsoft.Interop.Tests." + nameof(NativeExportsNE);
+
+        [GeneratedDllImport(NativeExportsNE_Binary, EntryPoint = "sumi")]
+        public static partial int Sum(int a, int b);
+
+        [GeneratedDllImport(NativeExportsNE_Binary, EntryPoint = "sumouti")]
+        public static partial void Sum(int a, int b, out int c);
+
+        [GeneratedDllImport(NativeExportsNE_Binary, EntryPoint = "sumrefi")]
+        public static partial void Sum(int a, ref int b);
+    }
+
+    public class BlittableTypes
+    {
+        [Benchmark]
+        public int IntByValue()
+        {
+            return NativeExportsNE.Sum(3, 42);
+        }
+
+        [Benchmark]
+        public int IntOutByRef()
+        {
+            NativeExportsNE.Sum(3, 42, out int result);
+            return result;
+        }
+    }
+}
