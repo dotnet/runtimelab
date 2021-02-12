2c8ff60fff7c462f155cf598e33fbd1a31aa2819
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2021-02-12 10:14:08 +0100

    Annotate a couple more marshal APIs (#678)
    And add intrinsic handling. Fixes #634.

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/InteropServices/Marshal.CoreRT.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/InteropServices/Marshal.CoreRT.cs
@@ -100,6 +100,7 @@ namespace System.Runtime.InteropServices
             }
         }
 
+        [RequiresDynamicCode("Marshalling code for the object might not be available. Use the DestroyStructure<T> overload instead.")]
         public static unsafe void DestroyStructure(IntPtr ptr, Type structuretype)
         {
             if (ptr == IntPtr.Zero)
@@ -136,6 +137,7 @@ namespace System.Runtime.InteropServices
             }
         }
 
+        [RequiresDynamicCode("Marshalling code for the object might not be available. Use the StructureToPtr<T> overload instead.")]
         public static unsafe void StructureToPtr(object structure, IntPtr ptr, bool fDeleteOld)
         {
             if (structure == null)
