0298d07048253c861beb2aa0219b74aec7fa8172
Anton Lapounov <antonl@microsoft.com>
2021-02-15 08:29:29 -0800

    Fix emitting CodeView information for locals (#689)

--- src/coreclr/tools/aot/ObjWriter/jitDebugInfo.h
+++ src/coreclr/tools/aot/ObjWriter/jitDebugInfo.h
@@ -2,11 +2,36 @@
 #define JIT_DEBUG_INFO_H
 
 typedef unsigned int DWORD;
-#define TARGET_AMD64 1
 
+#define PORTABILITY_WARNING(msg)
 #include "cordebuginfo.h"
-#include "cvconst.h"
-#include "llvm/DebugInfo/CodeView/SymbolRecord.h"
+
+// RegNum enumeration is architecture-specific and we need it for all
+// architectures we support.
+
+namespace X86 {
+#define TARGET_X86 1
+#include "cordebuginfo.h"
+#undef TARGET_X86
+}
+
+namespace Amd64 {
+#define TARGET_AMD64 1
+#include "cordebuginfo.h"
+#undef TARGET_AMD64
+}
+
+namespace Arm {
+#define TARGET_ARM 1
+#include "cordebuginfo.h"
+#undef TARGET_ARM
+}
+
+namespace Arm64 {
+#define TARGET_ARM64 1
+#include "cordebuginfo.h"
+#undef TARGET_ARM64
+}
 
 struct DebugLocInfo {
   int NativeOffset;
@@ -37,19 +62,5 @@ struct DebugEHClauseInfo {
                     TryOffset(TryOffset), TryLength(TryLength),
                     HandlerOffset(HandlerOffset), HandlerLength(HandlerLength) {}
 };
-
-typedef unsigned short CVRegMapping;
-
-#define CVREGDAT(p2, cv) cv
-
-const CVRegMapping cvRegMapAmd64[] = {
-    CVREGDAT(REGNUM_RAX, CV_AMD64_RAX), CVREGDAT(REGNUM_RCX, CV_AMD64_RCX),
-    CVREGDAT(REGNUM_RDX, CV_AMD64_RDX), CVREGDAT(REGNUM_RBX, CV_AMD64_RBX),
-    CVREGDAT(REGNUM_RSP, CV_AMD64_RSP), CVREGDAT(REGNUM_RBP, CV_AMD64_RBP),
-    CVREGDAT(REGNUM_RSI, CV_AMD64_RSI), CVREGDAT(REGNUM_RDI, CV_AMD64_RDI),
-    CVREGDAT(REGNUM_R8, CV_AMD64_R8),   CVREGDAT(REGNUM_R9, CV_AMD64_R9),
-    CVREGDAT(REGNUM_R10, CV_AMD64_R10), CVREGDAT(REGNUM_R11, CV_AMD64_R11),
-    CVREGDAT(REGNUM_R12, CV_AMD64_R12), CVREGDAT(REGNUM_R13, CV_AMD64_R13),
-    CVREGDAT(REGNUM_R14, CV_AMD64_R14), CVREGDAT(REGNUM_R15, CV_AMD64_R15)};
 
 #endif // JIT_DEBUG_INFO_H
