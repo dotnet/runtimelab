4089184ac5cee5146b690329d396fa3286124db1
Jan Kotas <jkotas@microsoft.com>
2021-02-18 15:32:49 -0800

    Fix build breaks - delete NativeRuntimeEventSource.Dummy.cs

--- src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/NativeRuntimeEventSource.cs
+++ src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/NativeRuntimeEventSource.cs
@@ -1,10 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
 namespace System.Diagnostics.Tracing
 {
     /// <summary>
@@ -27,9 +23,8 @@ namespace System.Diagnostics.Tracing
         // as you can't make a constructor partial.
         private NativeRuntimeEventSource(int _) { }
 
-
-        // On Mono, we don't have these keywords defined from the genRuntimeEventSources.py, so we need to manually define them here.
-#if MONO
+        // On CoreCLR, we have these keywords defined from the genRuntimeEventSources.py.
+#if !CORECLR
         public class Keywords
         {
             public const EventKeywords GCKeyword = (EventKeywords)0x1;
@@ -69,8 +64,9 @@ namespace System.Diagnostics.Tracing
             public const EventKeywords TypeDiagnosticKeyword = (EventKeywords)0x8000000000;
             public const EventKeywords JitInstrumentationDataKeyword = (EventKeywords)0x10000000000;
         }
-#endif // MONO
+#endif // !CORECLR
 
+#if FEATURE_PERFTRACING
         /// <summary>
         /// Dispatch a single event with the specified event ID and payload.
         /// </summary>
@@ -106,62 +102,6 @@ namespace System.Diagnostics.Tracing
                 childActivityID: &childActivityId,
                 args: decodedPayloadFields);
         }
-
-        [NonEvent]
-        [DllImport(RuntimeHelpers.QCall)]
-        internal static extern void LogThreadPoolWorkerThreadStart(uint ActiveWorkerThreadCount, uint RetiredWorkerThreadCount, ushort ClrInstanceID);
-
-        [NonEvent]
-        [DllImport(RuntimeHelpers.QCall)]
-        internal static extern void LogThreadPoolWorkerThreadStop(uint ActiveWorkerThreadCount, uint RetiredWorkerThreadCount, ushort ClrInstanceID);
-
-        [NonEvent]
-        [DllImport(RuntimeHelpers.QCall)]
-        internal static extern void LogThreadPoolWorkerThreadWait(uint ActiveWorkerThreadCount, uint RetiredWorkerThreadCount, ushort ClrInstanceID);
-
-        [NonEvent]
-        [DllImport(RuntimeHelpers.QCall)]
-        internal static extern void LogThreadPoolWorkerThreadAdjustmentSample(double Throughput, ushort ClrInstanceID);
-
-        [NonEvent]
-        [DllImport(RuntimeHelpers.QCall)]
-        internal static extern void LogThreadPoolWorkerThreadAdjustmentAdjustment(double AverageThroughput, uint NewWorkerThreadCount, NativeRuntimeEventSource.ThreadAdjustmentReasonMap Reason, ushort ClrInstanceID);
-
-        [NonEvent]
-        [DllImport(RuntimeHelpers.QCall)]
-        internal static extern void LogThreadPoolWorkerThreadAdjustmentStats(
-            double Duration,
-            double Throughput,
-            double ThreadPoolWorkerThreadWait,
-            double ThroughputWave,
-            double ThroughputErrorEstimate,
-            double AverageThroughputErrorEstimate,
-            double ThroughputRatio,
-            double Confidence,
-            double NewControlSetting,
-            ushort NewThreadWaveMagnitude,
-            ushort ClrInstanceID);
-
-        [NonEvent]
-        [DllImport(RuntimeHelpers.QCall)]
-        internal static extern void LogThreadPoolIOEnqueue(
-            IntPtr NativeOverlapped,
-            IntPtr Overlapped,
-            bool MultiDequeues,
-            ushort ClrInstanceID);
-
-        [NonEvent]
-        [DllImport(RuntimeHelpers.QCall)]
-        internal static extern void LogThreadPoolIODequeue(
-            IntPtr NativeOverlapped,
-            IntPtr Overlapped,
-            ushort ClrInstanceID);
-
-        [NonEvent]
-        [DllImport(RuntimeHelpers.QCall)]
-        internal static extern void LogThreadPoolWorkingThreadCount(
-            uint Count,
-            ushort ClrInstanceID
-        );
+#endif // FEATURE_PERFTRACING
     }
 }
