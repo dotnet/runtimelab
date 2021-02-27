c4a1405d8b2ea3f0c62fc20d4ce44cce19080efb
yowl <scott.waye@hubse.com>
2021-02-27 18:36:14 -0500

    update to llvm11 (#757)

--- src/coreclr/tools/aot/ILCompiler.LLVM/CodeGen/LLVMSharpInterop.cs
+++ src/coreclr/tools/aot/ILCompiler.LLVM/CodeGen/LLVMSharpInterop.cs
@@ -4,14 +4,6 @@ namespace Internal.IL
 {
     internal class LLVMSharpInterop
     {
-        /// <summary>
-        /// Workaround while waiting for https://github.com/microsoft/LLVMSharp/pull/141
-        /// </summary>
-        internal static unsafe uint ElementAtOffset(LLVMTargetDataRef targetDataRef, LLVMTypeRef structTypeRef, ulong offset)
-        {
-            return LLVM.ElementAtOffset(targetDataRef, structTypeRef, offset);
-        }
-
         ///
         /// Wrapper while waiting for https://github.com/microsoft/LLVMSharp/pull/144
         /// 
