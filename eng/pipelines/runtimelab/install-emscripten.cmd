881fd28dcb4038a4d421b69ef8780efbe4b6280b
yowl <scott.waye@hubse.com>
2021-03-02 19:28:28 -0500

    update emscripten

--- eng/pipelines/runtimelab/install-emscripten.cmd
+++ eng/pipelines/runtimelab/install-emscripten.cmd
@@ -5,14 +5,14 @@ git clone https://github.com/emscripten-core/emsdk.git
 
 cd emsdk
 rem checkout a known good version to avoid a random break when emscripten changes the top of tree.
-git checkout ec9c1c4
+git checkout 5ad9d72
 
 powershell -NoProfile -NoLogo -ExecutionPolicy ByPass -command "& """%~dp0update-machine-certs.ps1""" %*"
 
 set
 
 dir C:\python3.7.0
-call python emsdk.py install 2.0.8
+call python emsdk.py install 2.0.12
 if %errorlevel% NEQ 0 goto fail
 call emsdk activate 2.0.8
 if %errorlevel% NEQ 0 goto fail
