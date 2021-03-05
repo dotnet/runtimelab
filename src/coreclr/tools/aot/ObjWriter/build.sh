40a902fa905c709d5236fa9e4fff562d86a66084
Anton Lapounov <antonl@microsoft.com>
2021-03-05 14:38:09 -0800

    Build ObjWriter for Linux ARM64 (#772)

--- src/coreclr/tools/aot/ObjWriter/build.sh
+++ src/coreclr/tools/aot/ObjWriter/build.sh
@@ -6,15 +6,29 @@ RepoRoot="$2"
 BuildArch="$3"
 TargetArch="$4"
 BuildType="${5:-Release}"
+CompilerId="$(echo "$6" | tr "[:upper:]" "[:lower:]")"
 
 # Check that we have enough arguments
 if [ $# -lt 4 ]; then
-    echo "Usage: $(basename $0) ArtifactsDir RepoRoot BuildArch TargetArch [BuildType]"
+    echo "Usage: $(basename $0) ArtifactsDir RepoRoot BuildArch TargetArch [BuildType [CompilerId]]"
     exit 1
 fi
 
-if [ "$TargetArch" != "$BuildArch" ]; then
-    echo "Error: Cross-building of objwriter is not supported"
+if [ -z "$CompilerId" ]; then
+    Compiler=clang
+    CompilerMajorVer=
+    CompilerMinorVer=
+# Expecting a compiler id similar to -clang9 or -gcc10.2. See also eng/native/build-commons.sh.
+elif [[ "$CompilerId" =~ ^-?([a-z]+)(-?([0-9]+)(\.([0-9]+))?)?$ ]]; then
+    Compiler=${BASH_REMATCH[1]}
+    CompilerMajorVer=${BASH_REMATCH[3]}
+    CompilerMinorVer=${BASH_REMATCH[5]}
+    if [[ "$Compiler" == "clang" && -n "$CompilerMajorVer" && "$CompilerMajorVer" -le 6
+        && -z "$CompilerMinorVer" ]]; then
+        CompilerMinorVer=0
+    fi
+else
+    echo "Unexpected compiler identifier '$6'"
     exit 1
 fi
 
@@ -48,12 +62,19 @@ if [ "$PatchApplied" -ne 1 ]; then
 fi
 
 # Configure and build objwriter
-mkdir -p "build/$TargetArch"
-cd "build/$TargetArch" || exit 1
+mkdir -p "build/$TargetArch" || exit 1
 
-# Do not use the -S and -B options to support older CMake versions
-cmake ../../ \
-    -DCMAKE_TOOLCHAIN_FILE="$ScriptDir/toolchain.cmake" \
+if [ "$TargetArch" != "$BuildArch" ]; then
+    export CROSSCOMPILE=1
+fi
+
+# Script arguments:
+#   <path to top-level CMakeLists.txt> <path to intermediate directory> <architecture>
+#   <compiler> <compiler major version> <compiler minor version>
+#   [build flavor] [ninja] [scan-build] [cmakeargs]
+"${RepoRoot}eng/native/gen-buildsys.sh" \
+    "$PWD" "$PWD/build/$TargetArch" "$TargetArch" \
+    "$Compiler" "$CompilerMajorVer" "$CompilerMinorVer" "$BuildType" \
     -DCMAKE_BUILD_TYPE="$BuildType" \
     -DCMAKE_INSTALL_PREFIX=install \
     -DLLVM_BUILD_TOOLS=0 \
@@ -68,4 +89,4 @@ cmake ../../ \
     -DCORECLR_INCLUDE_DIR="${RepoRoot}src/coreclr/inc" \
     || exit 1
 
-cmake --build . --config "$BuildType" --target objwriter -j 10 || exit 1
+cmake --build "build/$TargetArch" --config "$BuildType" --target objwriter -j 10 || exit 1
