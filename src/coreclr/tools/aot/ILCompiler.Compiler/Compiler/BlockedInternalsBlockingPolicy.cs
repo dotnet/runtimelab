d66f8384159155a5a35477c922fb55a58d87b88a
Michal Strehovský <michals@microsoft.com>
2021-03-23 19:32:29 +0100

    Switch attribute equality to dotnet/runtime plan
    .NET Native injected methods into these to make equality not reflection based. dotnet/runtime just walks fields using reflection.
    
    It's about the equal amount of pain to deal with either one. The reflection based implementation should work for the mainstream framework scenarios. It will become more tricky if we want to make reflection metadata more pay for play in the future. The IL Linker suppressions on this implementation assume we need field metadata to operate. That's not the case in NativeAOT.

--- src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/BlockedInternalsBlockingPolicy.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/BlockedInternalsBlockingPolicy.cs
@@ -156,6 +156,7 @@ namespace ILCompiler
         private MetadataType ArrayOfTType { get; }
         private MetadataType SerializationInfoType { get; }
         private MetadataType ISerializableType { get; }
+        private MetadataType AttributeType { get; }
 
         public BlockedInternalsBlockingPolicy(TypeSystemContext context)
         {
@@ -164,6 +165,7 @@ namespace ILCompiler
             ArrayOfTType = context.SystemModule.GetType("System", "Array`1", false);
             SerializationInfoType = context.SystemModule.GetType("System.Runtime.Serialization", "SerializationInfo", false);
             ISerializableType = context.SystemModule.GetType("System.Runtime.Serialization", "ISerializable", false);
+            AttributeType = context.SystemModule.GetType("System", "Attribute", false);
         }
 
         public override bool IsBlocked(MetadataType type)
@@ -278,6 +280,18 @@ namespace ILCompiler
                 {
                     return false;
                 }
+
+                // Exempt fields on custom attributes from blocking. We use a heuristic instead of
+                // unconditionally walking the base class hierarchy as a perf optimization.
+                // Reflection blocking will be active for framework assemblies and those follow the
+                // attribute naming guidelines.
+                if (owningType.Name.EndsWith("Attribute")
+                    && AttributeType != null
+                    && owningType.CanCastTo(AttributeType))
+                {
+                    return false;
+                }
+
                 return true;
             }
 
