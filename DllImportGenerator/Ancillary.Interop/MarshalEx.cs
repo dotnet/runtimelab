03626917fe487c692e8534f571964b7807c48802
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-04-30 10:25:24 -0700

    Cleanup and comments.

--- DllImportGenerator/Ancillary.Interop/MarshalEx.cs
+++ DllImportGenerator/Ancillary.Interop/MarshalEx.cs
@@ -7,6 +7,8 @@ namespace System.Runtime.InteropServices
     /// <summary>
     /// Marshalling helper methods that will likely live in S.R.IS.Marshal
     /// when we integrate our APIs with dotnet/runtime.
+    /// When referencing this type in a source-generator, make sure to use
+    /// the TypeNames.MarshalEx method to enable configuration when dogfooding.
     /// </summary>
     public static class MarshalEx
     {
