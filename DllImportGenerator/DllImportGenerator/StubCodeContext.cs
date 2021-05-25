162494879fa39dbcd2b64acadb573f44c29d8e51
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-05-25 11:35:48 -0700

    Trim the '@' symbol from the start of the native identifier.

--- DllImportGenerator/DllImportGenerator/StubCodeContext.cs
+++ DllImportGenerator/DllImportGenerator/StubCodeContext.cs
@@ -93,7 +93,7 @@ namespace Microsoft.Interop
         /// <returns>Managed and native identifiers</returns>
         public virtual (string managed, string native) GetIdentifiers(TypePositionInfo info)
         {
-            return (info.InstanceIdentifier, $"__{info.InstanceIdentifier}{GeneratedNativeIdentifierSuffix}");
+            return (info.InstanceIdentifier, $"__{info.InstanceIdentifier.TrimStart('@')}{GeneratedNativeIdentifierSuffix}");
         }
 
         public abstract TypePositionInfo? GetTypePositionInfoForManagedIndex(int index);
