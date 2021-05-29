6076d83b35f470ddf0138a1964e8bddb764aae11
Andrii Kurdiumov <kant2002@gmail.com>
2021-05-29 16:54:48 +0600

    Address other small issues

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/InteropServices/Marshal.Com.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/InteropServices/Marshal.Com.cs
@@ -199,12 +199,11 @@ namespace System.Runtime.InteropServices
                     data->AsError = value.ErrorCode;
                     break;
                 case VariantWrapper value:
-                    // Do not want to implement that yet.
-                    throw new NotSupportedException();
+                    throw new ArgumentException();
                 case DBNull value:
                     data->SetAsNULL();
                     break;
-                case System.Reflection.Missing value:
+                case Missing value:
                     data->AsError = DISP_E_PARAMNOTFOUND;
                     break;
                 case IConvertible value:
@@ -220,53 +219,53 @@ namespace System.Runtime.InteropServices
                             data->SetAsNULL();
                             break;
                         case TypeCode.Boolean:
-                            data->AsBool = value.ToBoolean();
+                            data->AsBool = value.ToBoolean(null);
                             break;
                         case TypeCode.Char:
-                            data->AsUi2 = value.ToChar();
+                            data->AsUi2 = value.ToChar(null);
                             break;
                         case TypeCode.SByte:
-                            data->AsI1 = value.ToSByte();
+                            data->AsI1 = value.ToSByte(null);
                             break;
                         case TypeCode.Byte:
-                            data->AsUi1 = value.ToByte();
+                            data->AsUi1 = value.ToByte(null);
                             break;
                         case TypeCode.Int16:
-                            data->AsI2 = value.ToInt16();
+                            data->AsI2 = value.ToInt16(null);
                             break;
                         case TypeCode.UInt16:
-                            data->AsUi2 = value.ToUInt16();
+                            data->AsUi2 = value.ToUInt16(null);
                             break;
                         case TypeCode.Int32:
-                            data->AsI4 = value.ToInt32();
+                            data->AsI4 = value.ToInt32(null);
                             break;
                         case TypeCode.UInt32:
-                            data->AsUi4 = value.ToUInt32();
+                            data->AsUi4 = value.ToUInt32(null);
                             break;
                         case TypeCode.Int64:
-                            data->AsI8 = value.ToInt64();
+                            data->AsI8 = value.ToInt64(null);
                             break;
                         case TypeCode.UInt64:
-                            data->AsUi8 = value.ToUInt64();
+                            data->AsUi8 = value.ToUInt64(null);
                             break;
                         case TypeCode.Single:
-                            data->AsR4 = value.ToSingle();
+                            data->AsR4 = value.ToSingle(null);
                             break;
                         case TypeCode.Double:
-                            data->AsR8 = value.ToDouble();
+                            data->AsR8 = value.ToDouble(null);
                             break;
                         case TypeCode.Decimal:
-                            data->AsDecimal = value.ToDecimal();
+                            data->AsDecimal = value.ToDecimal(null);
                             break;
                         case TypeCode.DateTime:
-                            data->AsDate = value.ToDateTime();
+                            data->AsDate = value.ToDateTime(null);
                             break;
                         case TypeCode.String:
                             data->AsBstr = value.ToString();
                             break;
                         default:
                             throw new NotSupportedException();
-                    } 
+                    }
                     break;
                 case CriticalHandle:
                     throw new ArgumentException();
