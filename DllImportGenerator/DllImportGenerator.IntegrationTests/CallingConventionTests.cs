fa6cc89f398589b83d809f8787596cd2b326e3b6
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-06-17 14:57:31 -0700

    WIP add tests and update .NET CLI version to a nightly of 6.0 Preview 6

---
+++ DllImportGenerator/DllImportGenerator.IntegrationTests/CallingConventionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllImportGenerator.IntegrationTests
+{
+    internal partial class NativeExportsNE
+    {
+        internal partial class CallingConventions
+        {
+            [GeneratedDllImport(NativeExportsNE_Binary)]
+            [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+            public static partial long AddLongsCdecl(long i, long j, long k, long l, long m, long n, long o, long p, long q);
+            [GeneratedDllImport(NativeExportsNE_Binary)]
+            [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvStdcall) })]
+            public static partial long AddLongsStdcall(long i, long j, long k, long l, long m, long n, long o, long p, long q);
+        }
+    }
+
+    public class CallingConventionTests
+    {
+    }
+}
