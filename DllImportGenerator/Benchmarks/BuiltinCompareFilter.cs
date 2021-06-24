dd13ad06392c74b7ee97eb78fd77c57b3b7bf6f7
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-06-23 17:06:20 -0700

    Add infra for generator-only benchmarks (#1275)

---
+++ DllImportGenerator/Benchmarks/BuiltinCompareFilter.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Filters;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    /// <summary>
+    /// Filter benchmarks to only benchmarks that work with the built-in and source-generated stubs.
+    /// </summary>
+    class BuiltinCompareFilter : IFilter
+    {
+        private AnyCategoriesFilter includeFilter = new AnyCategoriesFilter(new[] { CategoryNames.DllImportGeneratorOnly });
+
+        public bool Predicate(BenchmarkCase benchmarkCase)
+        {
+            return !includeFilter.Predicate(benchmarkCase);
+        }
+    }
+}
