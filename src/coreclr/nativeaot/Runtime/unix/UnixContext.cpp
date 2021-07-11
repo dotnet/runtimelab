36415088e9ba237243e7b7f61c18b044a9022d91
Andrii Kurdiumov <kant2002@gmail.com>
2021-07-11 13:09:44 +0600

    Remove WASM specific defines which was added for compatibility with unw_get_save_loc which is removed now in latest libunwins.

--- src/coreclr/nativeaot/Runtime/unix/UnixContext.cpp
+++ src/coreclr/nativeaot/Runtime/unix/UnixContext.cpp
@@ -19,34 +19,6 @@
 #include "UnixContext.h"
 #include "UnwindHelpers.h"
 
-// WebAssembly has a slightly different version of LibUnwind that doesn't define unw_get_save_loc
-#if defined(HOST_WASM)
-enum unw_save_loc_type_t
-{
-    UNW_SLT_NONE,       /* register is not saved ("not an l-value") */
-    UNW_SLT_MEMORY,     /* register has been saved in memory */
-    UNW_SLT_REG         /* register has been saved in (another) register */
-};
-typedef enum unw_save_loc_type_t unw_save_loc_type_t;
-
-struct unw_save_loc_t
-{
-    unw_save_loc_type_t type;
-    union
-    {
-        unw_word_t addr;        /* valid if type==UNW_SLT_MEMORY */
-        unw_regnum_t regnum;    /* valid if type==UNW_SLT_REG */
-    }
-    u;
-};
-typedef struct unw_save_loc_t unw_save_loc_t;
-
-int unw_get_save_loc(unw_cursor_t*, int, unw_save_loc_t*)
-{
-    return -1;
-}
-#endif // _WASM
-
 #ifdef __APPLE__
 
 #define MCREG_Rip(mc)       ((mc)->__ss.__rip)
