b106afa9470bf3c12d241020a10059b0feb08aa0
Eric StJohn <ericstj@microsoft.com>
2021-08-04 16:31:20 -0700

    Sample compat package for System.Data.Linq

---
+++ src/System.Data.Linq/tests/BinaryTests.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System.Data.Linq
+{
+    public class RequestContextTests
+    {
+        [Fact]
+        public void BinaryDefaultCtor()
+        {
+            Binary bin = new Binary( new byte[] { 0xDE, 0xAD, 0xF0, 0x0D });
+        }
+    }
+}
