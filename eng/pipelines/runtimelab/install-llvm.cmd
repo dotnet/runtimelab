71e0a32a8ab5022b0b4b487f05d0297dcc73ca49
yowl <scott.waye@hubse.com>
2021-08-23 13:50:26 -0500

    Attempt to fix llvm install

--- eng/pipelines/runtimelab/install-llvm.cmd
+++ eng/pipelines/runtimelab/install-llvm.cmd
@@ -5,8 +5,7 @@ set RepoRoot=%2\
 
 set
 :: Set CMakePath by evaluating the output from set-cmake-path.ps1
-call "%RepoRoot%src\coreclr\setup_vs_tools.cmd" || exit /b 1
-for /f "delims=" %%a in ('powershell -NoProfile -ExecutionPolicy ByPass "& ""%RepoRoot%eng\native\set-cmake-path.ps1"""') do %%a
+call "%RepoRoot%eng\native\init-vs-env.cmd" || exit /b 1
 echo Using CMake at "%CMakePath%"
 
 set
