1f771ae06c7436a61cebd04098f1598f6d9a4b66
tlakollo <tlakaelel_axayakatl@outlook.com>
2021-12-27 16:27:41 -0800

    Add ILLink.Shared project to NativeAOT Change diagnostics to use ILLink.Shared utilities Update ReferenceSource

---
+++ src/coreclr/tools/aot/ILLink.Shared/DiagnosticString.cs
@@ -0,0 +1,32 @@
+﻿namespace ILLink.Shared
+{
+    public readonly struct DiagnosticString
+    {
+        readonly string _titleFormat;
+        readonly string _messageFormat;
+
+        public DiagnosticString(DiagnosticId diagnosticId)
+        {
+            var resourceManager = SharedStrings.ResourceManager;
+            _titleFormat = resourceManager.GetString($"{diagnosticId}Title") ?? string.Empty;
+            _messageFormat = resourceManager.GetString($"{diagnosticId}Message") ?? string.Empty;
+        }
+
+        public DiagnosticString(string diagnosticResourceStringName)
+        {
+            var resourceManager = SharedStrings.ResourceManager;
+            _titleFormat = resourceManager.GetString($"{diagnosticResourceStringName}Title") ?? string.Empty;
+            _messageFormat = resourceManager.GetString($"{diagnosticResourceStringName}Message") ?? string.Empty;
+        }
+
+        public string GetMessage(params string[] args) =>
+            string.Format(_messageFormat, args);
+
+        public string GetMessageFormat() => _messageFormat;
+
+        public string GetTitle(params string[] args) =>
+            string.Format(_titleFormat, args);
+
+        public string GetTitleFormat() => _titleFormat;
+    }
+}
---
+++ src/coreclr/tools/aot/ILLink.Shared/MessageFormat.cs
@@ -0,0 +1,36 @@
+﻿#nullable enable
+
+namespace ILLink.Shared
+{
+    internal static class MessageFormat
+    {
+        public static string FormatRequiresAttributeMessageArg(string? message)
+        {
+            if (!string.IsNullOrEmpty(message))
+                return $" {message}{(message!.TrimEnd().EndsWith(".") ? "" : ".")}";
+
+            return string.Empty;
+        }
+
+        public static string FormatRequiresAttributeUrlArg(string? url)
+        {
+            if (!string.IsNullOrEmpty(url))
+                return $" {url}";
+
+            return string.Empty;
+        }
+
+        public static string FormatRequiresAttributeMismatch(bool memberHasAttribute, bool isInterface, params object[] args)
+        {
+            string format = (memberHasAttribute, isInterface) switch
+            {
+                (false, true) => SharedStrings.InterfaceRequiresMismatchMessage,
+                (true, true) => SharedStrings.ImplementationRequiresMismatchMessage,
+                (false, false) => SharedStrings.BaseRequiresMismatchMessage,
+                (true, false) => SharedStrings.DerivedRequiresMismatchMessage
+            };
+
+            return string.Format(format, args);
+        }
+    }
+}
