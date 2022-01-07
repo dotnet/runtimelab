fb36369b1b755977b3ff0bcb24d90a0aec0f40db
yowl <scott.waye@hubse.com>
2022-01-07 08:18:24 -0500

    Build LLVM for Release config
    temp comment other builds

--- eng/pipelines/runtimelab/install-llvm.ps1
+++ eng/pipelines/runtimelab/install-llvm.ps1
@@ -1,3 +1,7 @@
+param (
+    [string]$buildConfig = "Release"
+)
+
 # LLVM is supplied in a gz file which Windows doesn't native understand, so we need gz to unpack it - TODO this is liable to fail randomly when a new version comes out and the version number changes
 Invoke-WebRequest -Uri https://tukaani.org/xz/xz-5.2.5-windows.zip -OutFile xz.zip
 Expand-Archive -LiteralPath xz.zip -DestinationPath .
@@ -12,9 +16,15 @@ cd llvm-11.0.0.src
 mkdir build
 cd build
 
-# TODO Release build
-& "$env:CMakePath" -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Debug -DLLVM_USE_CRT_DEBUG=MTd -Thost=x64 ..
+if($buildConfig -eq "Release")
+{
+    & "$env:CMakePath" -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_CRT_DEBUG=MTd -Thost=x64 ..
+}
+else
+{
+    & "$env:CMakePath" -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Debug -LLVM_USE_CRT_RELEASE=MT -Thost=x64 ..
+}
 
-& "$env:CMakePath" --build . --target LLVMCore
-& "$env:CMakePath" --build . --target LLVMBitWriter
+& "$env:CMakePath" --build . --config $buildConfig --target LLVMCore
+& "$env:CMakePath" --build . --config $buildConfig --target LLVMBitWriter
 #& "$env:CMakePath" --build . --target LLVMDebugInfoDwarf
