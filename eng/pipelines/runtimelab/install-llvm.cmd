90664207c7ad6b5c82a4887d0291ae21d84a5174
yowl <scott.waye@hubse.com>
2022-01-12 23:27:45 -0500

    Use Debug .
    Odd as thought it would be pulled from the LLVM config var.

--- eng/pipelines/runtimelab/install-llvm.cmd
+++ eng/pipelines/runtimelab/install-llvm.cmd
@@ -13,8 +13,8 @@ echo Using CMake at "%CMakePath%"
 
 set
 
-REM There is no [C/c]hecked LLVM config, so change to Release
-if /I %buildConfig% EQU checked set buildConfig=Release
+REM There is no [C/c]hecked LLVM config, so change to Debug
+if /I %buildConfig% EQU checked set buildConfig=Debug
 
 powershell -NoProfile -NoLogo -ExecutionPolicy ByPass -File "%~dp0install-llvm.ps1" -buildConfig %buildConfig%
 if %errorlevel% NEQ 0 goto fail
