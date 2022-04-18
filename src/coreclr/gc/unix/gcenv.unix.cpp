53adb34ece60a9ac38fee5069a2778a8e620b8cf
yowl <scott.waye@hubse.com>
2022-04-18 17:04:15 -0500

    revert changes - don't seem to be required.

--- src/coreclr/gc/unix/gcenv.unix.cpp
+++ src/coreclr/gc/unix/gcenv.unix.cpp
@@ -1325,9 +1325,6 @@ void GCToOSInterface::GetMemoryStatus(uint64_t restricted_limit, uint32_t* memor
         {
             available = GetAvailablePhysicalMemory();
 
-#if TARGET_WASM
-            load = 50;
-#else
             if (memory_load != NULL)
             {
                 bool isRestricted;
@@ -1339,7 +1336,6 @@ void GCToOSInterface::GetMemoryStatus(uint64_t restricted_limit, uint32_t* memor
                     load = (uint32_t)(((float)used * 100) / (float)total);
                 }
             }
-#endif
         }
     }
 
