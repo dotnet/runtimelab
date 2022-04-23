b67eb9a631055b6f00c783b636bb6661e55e635d
yowl <scott.waye@hubse.com>
2022-04-23 08:24:58 -0500

    NativeAOT-LLVM: Emsdk update to 3.1.8 (#1885)
    * update emscripten fix for https://reviews.llvm.org/D114787 - disable -Wno-declaration-after-statement
    
    * get fix for python path

--- eng/pipelines/runtimelab/install-emscripten.cmd
+++ eng/pipelines/runtimelab/install-emscripten.cmd
@@ -5,13 +5,13 @@ git clone https://github.com/emscripten-core/emsdk.git
 
 cd emsdk
 rem Checkout a known good version to avoid a random break when emscripten changes the top of tree.
-git checkout 447aa44
+git checkout e23aac7
 
 powershell -NoProfile -NoLogo -ExecutionPolicy ByPass -command "& """%~dp0update-machine-certs.ps1""" %*"
 
-call python emsdk.py install 2.0.33
+call python emsdk.py install 3.1.8
 if %errorlevel% NEQ 0 goto fail
-call emsdk activate 2.0.33
+call emsdk activate 3.1.8
 if %errorlevel% NEQ 0 goto fail
 
 rem We key off of this variable in the common/build.ps1 script.
