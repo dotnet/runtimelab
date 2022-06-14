2a4ff0f852851874b605922e7c7fed6aae17eb4c
Jose Perez Rodriguez <joperezr@microsoft.com>
2022-06-14 14:32:04 -0700

    Apply suggestions from code review

--- src/Experiment/src/AssemblyBuilder.cs
+++ src/Experiment/src/AssemblyBuilder.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,30 +8,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
 
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
 namespace System.Reflection.Emit.Experimental
 {
-   public class AssemblyBuilder: System.Reflection.Assembly
+    public class AssemblyBuilder: System.Reflection.Assembly
     {
         internal AssemblyBuilder() { }
 
         public void Save(string assemblyFileName)
         {
-//New method
+            //New method
         }
 
         public static System.Reflection.Emit.AssemblyBuilder DefineDynamicAssembly(System.Reflection.AssemblyName name, System.Reflection.Emit.AssemblyBuilderAccess access) 
         { 
             throw new NotImplementedException(); 
-        }// code forthecoming
+        }
 
         [System.Diagnostics.CodeAnalysis.RequiresDynamicCode("Defining a dynamic assembly requires dynamic code.")]
         public static System.Reflection.Emit.AssemblyBuilder DefineDynamicAssembly(System.Reflection.AssemblyName name, System.Reflection.Emit.AssemblyBuilderAccess access, System.Collections.Generic.IEnumerable<System.Reflection.Emit.CustomAttributeBuilder>? assemblyAttributes) 
         { 
             throw new NotImplementedException(); 
         }
+
         public System.Reflection.Emit.ModuleBuilder DefineDynamicModule(string name) 
         { 
             throw new NotImplementedException(); 
