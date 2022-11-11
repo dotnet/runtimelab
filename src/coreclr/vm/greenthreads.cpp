dotnet/runtimelab

commit be2df4a1e76cc1e44d9b1b702812232145982edd

Introducing GreenThreadFrame to allow stack walking across the boundary

src/coreclr/vm/greenthreads.cpp
@@ -153,8 +153,19 @@ extern "C" uintptr_t FirstFrameInGreenThreadCpp(TransitionHelperFunction functio
 {
     GetThread()->SetExecutingOnAltStack();
     assert(t_greenThread.inGreenThread);
+    FrameWithCookie<GreenThreadFrame> f;
+
+    {
+        GCX_COOP();
+        f.Push(GetThread());
+    }
 
     uintptr_t result = param->function(param->param);
+
+    {
+        // TODO: Pop()
+    }
+
     t_greenThread.greenThreadStackCurrent = NULL;
 
     return result;

src/coreclr/debug/daccess/dacdbiimplstackwalk.cpp
@@ -302,6 +302,10 @@ HRESULT DacDbiInterfaceImpl::CheckContext(VMPTR_Thread       vmThread,
         return S_OK;
     }
 
+#ifndef FEATURE_GREENTHREADS
+
+    // TODO, AndrewAu, ideally, we should skip this check only if we knew this is currently
+    // a green thread, but disabling it always will suffice, it is currently only used in asserts
     if (!g_fSkipStackCheckInit)
     {
         g_fSkipStackCheck = (CLRConfig::GetConfigValue(CLRConfig::UNSUPPORTED_DbgSkipStackCheck) != 0);
@@ -324,6 +328,8 @@ HRESULT DacDbiInterfaceImpl::CheckContext(VMPTR_Thread       vmThread,
             return CORDBG_E_NON_MATCHING_CONTEXT;
         }
     }
+
+#endif //FEATURE_GREENTHREADS
 
     return S_OK;
 }

src/coreclr/inc/vptr_list.h
@@ -91,6 +91,9 @@ VPTR_CLASS(ExceptionFilterFrame)
 #ifdef _DEBUG
 VPTR_CLASS(AssumeByrefFromJITStack)
 #endif
+#ifdef FEATURE_GREENTHREADS
+VPTR_CLASS(GreenThreadFrame)
+#endif
 
 #ifdef DEBUGGING_SUPPORTED
 VPTR_CLASS(Debugger)

src/coreclr/vm/frames.h
@@ -234,6 +234,9 @@ FRAME_TYPE_NAME(ExceptionFilterFrame)
 #if defined(_DEBUG)
 FRAME_TYPE_NAME(AssumeByrefFromJITStack)
 #endif // _DEBUG
+#ifdef FEATURE_GREENTHREADS
+FRAME_TYPE_NAME(GreenThreadFrame)
+#endif // FEATURE_GREENTHREADS
 
 #undef FRAME_ABSTRACT_TYPE_NAME
 #undef FRAME_TYPE_NAME
@@ -3121,6 +3124,20 @@ private:
 }; //AssumeByrefFromJITStack
 
 #endif //_DEBUG
+
+#ifdef FEATURE_GREENTHREADS
+class GreenThreadFrame : public Frame
+{
+    VPTR_VTABLE_CLASS(GreenThreadFrame, Frame)
+public:
+    GreenThreadFrame()
+    {
+    }
+
+    // Keep as last entry in class
+    DEFINE_VTABLE_GETTER_AND_DTOR(GreenThreadFrame)
+};
+#endif
 
 //-----------------------------------------------------------------------------
 // FrameWithCookie is used to declare a Frame in source code with a cookie

commit ce4413aedef85c4c609500ba9922433945bc9403

Comment out the new frame in FirstFrameInGreenThreadCpp for now
- Yields appear to be failing (returning false) with that frame, seems to have started happening after be2df4a1e76cc1e44d9b1b702812232145982edd
- Commented out the push/pop of the new frame for now

src/coreclr/vm/greenthreads.cpp
@@ -153,19 +153,19 @@ extern "C" uintptr_t FirstFrameInGreenThreadCpp(TransitionHelperFunction functio
 {
     GetThread()->SetExecutingOnAltStack();
     assert(t_greenThread.inGreenThread);
-    FrameWithCookie<GreenThreadFrame> f;
+    //FrameWithCookie<GreenThreadFrame> f;
 
-    {
-        GCX_COOP();
-        f.Push(GetThread());
-    }
+    //{
+    //    GCX_COOP();
+    //    f.Push(GetThread());
+    //}
 
     uintptr_t result = param->function(param->param);
 
-    {
-        GCX_COOP();
-        f.Pop(GetThread());
-    }
+    //{
+    //    GCX_COOP();
+    //    f.Pop(GetThread());
+    //}
 
     t_greenThread.greenThreadStackCurrent = NULL;
 