b8ee43c1b8b9e9e8b34624f0d9538e9bfe70359b
David Wrighton <davidwr@microsoft.com>
2022-11-16 15:09:27 -0800

    actually add test change

--- src/tests/baseservices/threading/greenthreads/deep/simple.cs
+++ src/tests/baseservices/threading/greenthreads/deep/simple.cs
@@ -19,18 +19,43 @@ public class Test_greenthread_delay {
     }
 
     static int printedout = 0;
+    class TempObj
+    {
+        static int tempObjs;
+        public TempObj()
+        {
+            o = Interlocked.Increment(ref tempObjs);
+        }
+        public object o;
+    }
+
+    static TempObj tmpObjStore;
+
     static void Deep(int moredeep)
     {
+        TempObj tmp = new TempObj();
+        tmpObjStore = tmp;
+        object objLocal = tmp.o;
+        int localNumVal = (int)objLocal;
+
         if (moredeep <= 0)
         {
             printedout++;
             Console.WriteLine("SuperDeep");
+            GC.Collect();
             Task.Delay(2000).Wait();
             Console.WriteLine("SuperDeepResumed");
             return;
         }
+
         Deep(moredeep - 1);
         if (printedout < 10)
             Deep(moredeep - 1);
+
+        if (localNumVal != (int)objLocal)
+            Console.WriteLine("GC fail");
+
+        if (!Object.ReferenceEquals(objLocal, tmp.o))
+            Console.WriteLine("GC fail2");
     }
 }
