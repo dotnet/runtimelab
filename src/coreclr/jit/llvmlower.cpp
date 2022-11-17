fe45b0fc93643669759fd4ef2b8906ba6bf48948
SingleAccretion <AccretionMail@yandex.ru>
2022-11-17 20:25:17 +0300

    Allow TYP_BLK variables

--- src/coreclr/jit/llvmlower.cpp
+++ src/coreclr/jit/llvmlower.cpp
@@ -11,6 +11,13 @@
 //
 void Llvm::Lower()
 {
+    // TODO-LLVM: enable. Currently broken because RyuJit inserts RPI helpers for RPI methods, then we
+    // also create an RPI wrapper stub, resulting in a double transition.
+    if (_compiler->opts.IsReversePInvoke())
+    {
+        failFunctionCompilation();
+    }
+
     populateLlvmArgNums();
 
     _shadowStackLocalsSize = 0;
