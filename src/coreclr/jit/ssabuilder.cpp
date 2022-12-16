d8b705629d4be46c69f375fcc2a291dd1435a336
yowl <scott.waye@hubse.com>
2022-12-16 12:14:17 -0500

    Update src/coreclr/jit/ssabuilder.cpp
    Co-authored-by: SingleAccretion <62474226+SingleAccretion@users.noreply.github.com>

--- src/coreclr/jit/ssabuilder.cpp
+++ src/coreclr/jit/ssabuilder.cpp
@@ -1395,7 +1395,7 @@ void SsaBuilder::AddPhiArgsToSuccessors(BasicBlock* block)
             // TODO-LLVM: LIR memory liveness is NYI upstream. Delete when that is fixed.
             if (block->IsLIR())
             {
-                return;
+                break;
             }
 #endif // TARGET_WASM
 
