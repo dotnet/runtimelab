1d4d1ca13aadd2c2a5a028a23db7e713fe2d8f7f
yowl <scott.waye@hubse.com>
2023-04-14 17:00:38 -0500

    didn't save before committing, sorry.

--- src/coreclr/jit/ssabuilder.cpp
+++ src/coreclr/jit/ssabuilder.cpp
@@ -848,15 +848,14 @@ void SsaBuilder::RenameDef(GenTree* defNode, BasicBlock* block)
         isLocal = defNode->OperIsLocalStore();
         lclNode = isLocal ? defNode->AsLclVarCommon() : nullptr;
         isFullDef = !lclNode->IsPartialLclFld(m_pCompiler);
+        offset = lclNode->GetLclOffs();
 
         if (lclNode->OperIs(GT_STORE_LCL_FLD))
         {
-            offset = lclNode->AsLclFld()->GetLclOffs();
             storeSize = lclNode->AsLclFld()->GetSize();
         }
         else
         {
-            offset = lclNode->GetLclOffs();
             storeSize = m_pCompiler->lvaLclExactSize(lclNode->GetLclNum());
         }
     }
