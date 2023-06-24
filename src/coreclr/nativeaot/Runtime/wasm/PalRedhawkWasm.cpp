9a582d06434b93a453815ff592bcbea2a474c92f
yowl <scott.waye@hubse.com>
2023-06-24 13:27:38 -0500

    switch target to wasm32-wasi
    add stubs for missing functions

--- src/coreclr/nativeaot/Runtime/wasm/PalRedhawkWasm.cpp
+++ src/coreclr/nativeaot/Runtime/wasm/PalRedhawkWasm.cpp
@@ -39,4 +39,101 @@ void PalGetMaximumStackBounds_SingleThreadedWasm(void** ppStackLowOut, void** pp
     *ppStackLowOut = pStackLow;
     *ppStackHighOut = pStackHigh;
 }
+
+#ifdef TARGET_WASI
+// nop stubs
+
+int pthread_mutex_init(pthread_mutex_t * m, const pthread_mutexattr_t *__restrict a)
+{
+    return 0;
+}
+
+int pthread_mutexattr_init(pthread_mutexattr_t *a)
+{
+    return 0;
+}
+
+int pthread_mutexattr_settype(pthread_mutexattr_t *a, int type)
+{
+    return 0;
+}
+
+int pthread_mutex_destroy(pthread_mutex_t *mutex)
+{
+    return 0;
+}
+
+int pthread_mutexattr_destroy(pthread_mutexattr_t *)
+{
+    return 0;
+}
+
+int pthread_cond_init(pthread_cond_t *cond, const pthread_condattr_t *attr)
+{
+    return 0;
+}
+
+int pthread_condattr_init(pthread_condattr_t *attr)
+{
+    return 0;
+}
+
+int pthread_mutex_lock(pthread_mutex_t *)
+{
+    return 0;
+}
+
+int pthread_mutex_unlock(pthread_mutex_t *)
+{
+    return 0;
+}
+
+pthread_t pthread_self(void)
+{
+    return (pthread_t)0;
+}
+
+int pthread_equal(pthread_t, pthread_t)
+{
+    return 1; // only one thread
+}
+
+int pthread_attr_init(pthread_attr_t *)
+{
+    return 0;
+}
+
+int pthread_attr_destroy(pthread_attr_t *)
+{
+    return 0;
+}
+
+int pthread_condattr_destroy(pthread_condattr_t *)
+{
+    return 0;
+}
+
+int pthread_cond_broadcast(pthread_cond_t *)
+{
+    return 0;
+}
+
+int pthread_attr_setdetachstate(pthread_attr_t *, int)
+{
+    return 0;
+}
+
+#include <sys/mman.h>
+int mprotect(void* addr, size_t len, int prot)
+{
+    return 0;
+}
+
+extern "C" int __cxa_thread_atexit(void (*func)(), void*, void*)
+{
+    return 0;
+}
+
+#endif // TARGET_WASI
+
 #endif // !FEATURE_WASM_THREADS
