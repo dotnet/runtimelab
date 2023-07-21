4e7cc9f1fc63b5828d1ec808ba1bc106587811b6
VivianaDuenas <136634758+VivianaDuenas@users.noreply.github.com>
2023-07-21 11:54:01 -0700

    PR feedback

--- src/Microsoft.ManagedZLib/tests/DeflateStreamTests.cs
+++ src/Microsoft.ManagedZLib/tests/DeflateStreamTests.cs
@@ -16,21 +16,10 @@ public class DeflateStreamTests
  
     public static IEnumerable<object[]> UncompressedTestFiles()
     {
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "TestDocument.doc") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "TestDocument.docx") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "TestDocument.pdf") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "TestDocument.txt") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "alice29.txt") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "asyoulik.txt") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "cp.html") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "fields.c") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "grammar.lsp") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "kennedy.xls") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "lcet10.txt") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "plrabn12.txt") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "ptt5") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "sum") };
-        yield return new object[] { Path.Combine("UncompressedTestFiles", "xargs.1") };
+        foreach (var path in Directory.EnumerateFiles("UncompressedTestFiles", "*", SearchOption.AllDirectories))
+        {
+            yield return new object[] { path };
+        }        
     }
 
     public static IEnumerable<object[]> ByteArrayData()
