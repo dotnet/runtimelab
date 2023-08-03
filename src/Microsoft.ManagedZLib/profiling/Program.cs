21c55fa72cad4139c51c2bbf6c1ffc8953481ecb
VivianaDuenas <136634758+VivianaDuenas@users.noreply.github.com>
2023-08-03 14:27:47 -0700

    Profiling console app.

---
+++ src/Microsoft.ManagedZLib/profiling/Program.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.ManagedZLib.Benchmarks;
+using System;
+using System.IO;
+
+namespace Microsoft.ManagedZLib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CompressedFile compressedFile = new("sum", System.IO.Compression.CompressionLevel.Optimal);
+        compressedFile.CompressedDataStream.Position = 0;
+        MemoryStream expectedStream = new();
+        DeflateStream decompressor = new(compressedFile.CompressedDataStream, CompressionMode.Decompress);
+
+        for (int i = 0; i < 10_000; i++)
+        {
+            Console.WriteLine($"Iteration: {i}");
+            decompressor.CopyTo(expectedStream);
+            compressedFile.CompressedDataStream.Position = 0;
+            expectedStream.Position = 0;
+        }
+    }
+}
