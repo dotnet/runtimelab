5f293f761172bdbe376efa5e7894ea2e9ae0b88d
Viviana Duenas Chavez <t-vivianadue@microsoft.com>
2023-08-16 14:34:23 -0700

    For Repro

--- src/Microsoft.ManagedZLib/profiling/Program.cs
+++ src/Microsoft.ManagedZLib/profiling/Program.cs
@@ -13,53 +13,53 @@ namespace Microsoft.ManagedZLib;
 
 internal class Program
 {
+
+    static Stopwatch stopWatch = new Stopwatch();
+    static CompressedFile compressedFile = new("TestDocument.pdf", System.IO.Compression.CompressionLevel.SmallestSize);
+    static MemoryStream expectedStream = new();
+    //System.IO.Compression.DeflateStream decompressor = new(compressedFile.CompressedDataStream, System.IO.Compression.CompressionMode.Decompress);
+
     [MethodImpl(MethodImplOptions.NoInlining)]
-    static void TestM(MemoryStream expectedStream, CompressedFile compressedFile, DeflateStream decompressor)
+    static void TestM(DeflateStream decompressor)
     {
-        //Console.WriteLine($"Iteration: {i}");
-        decompressor.CopyTo(expectedStream);
         compressedFile.CompressedDataStream.Position = 0;
         expectedStream.Position = 0;
+        decompressor.CopyTo(expectedStream); 
     }
     static void TestN(MemoryStream expectedStream, CompressedFile compressedFile, System.IO.Compression.DeflateStream decompressor)
     {
-        //Console.WriteLine($"Iteration: {i}");
-        decompressor.CopyTo(expectedStream);
         compressedFile.CompressedDataStream.Position = 0;
         expectedStream.Position = 0;
+        decompressor.CopyTo(expectedStream);
     }
 
 
     private static void Main(string[] args)
     {
-        int iter = 20_000_000;
-        Stopwatch stopWatch = new Stopwatch();
-        CompressedFile compressedFile = new("TestDocument.pdf", System.IO.Compression.CompressionLevel.SmallestSize);
+        int iter = 65_536;
         compressedFile.CompressedDataStream.Position = 0;
-        MemoryStream expectedStream = new();
+        DeflateStream decompressor = new(compressedFile.CompressedDataStream, CompressionMode.Decompress);
 
         Console.WriteLine($"Environment.Version: {Environment.Version}");
         Console.WriteLine($"RuntimeInformation.FrameworkDescription: {RuntimeInformation.FrameworkDescription}");
         Console.WriteLine($"AssemblyFileVersion: {typeof(object).Assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()!.Version!}");
 
-        DeflateStream decompressor = new(compressedFile.CompressedDataStream, CompressionMode.Decompress);
-        //System.IO.Compression.DeflateStream decompressor = new(compressedFile.CompressedDataStream, System.IO.Compression.CompressionMode.Decompress);
         for (int i = 0; i < 100; i++)
         {
-            TestM(expectedStream, compressedFile, decompressor);
+            TestM(decompressor);
         }
 
         stopWatch.Start();
         for (int i = 0; i < iter; i++)
         {
-            TestM(expectedStream, compressedFile, decompressor);
+            TestM(decompressor);
         }
         stopWatch.Stop();
 
         // Elapsed time as a TimeSpan value.
         TimeSpan ts = stopWatch.Elapsed;
-        Console.WriteLine($"Total time elapsed (s): {ts.TotalMicroseconds}");
+        Console.WriteLine($"Total time elapsed (us): {ts.TotalMicroseconds}");
         double res = ts.TotalMicroseconds / iter;
-        Console.WriteLine($"Time elapsed per iteration(s): {res}");
+        Console.WriteLine($"Time elapsed per iteration(us): {res}");
     }
 }
