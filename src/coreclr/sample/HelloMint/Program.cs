fcdbf34ee71e52e8d37fccfa11503f9e91cc70c1
Ivan Povazan <ivan.povazan@gmail.com>
2023-09-11 16:57:43 +0200

    Adding in-tree sample for minting

---
+++ src/coreclr/sample/HelloMint/Program.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace HelloMint
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (AppContext.TryGetSwitch("System.Private.Mint.Enable", out var enabled))
+            {
+                Console.WriteLine ("Hello, Mint is {0}", enabled ? "enabled": "disabled");
+            }
+            else
+            {
+                Console.WriteLine ($"Hello, System.Private.Mint.Enable is unset");
+            }
+        }
+    }
+}
