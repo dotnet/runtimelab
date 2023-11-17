7f16d2518b3793f47c360eba09dae34d598529a6
vsadov <8218165+VSadov@users.noreply.github.com>
2023-11-16 17:39:59 -0800

    Updated the test

--- src/tests/Loader/async/async2gcRootsScan.cs
+++ src/tests/Loader/async/async2gcRootsScan.cs
@@ -10,8 +10,8 @@ public class Async2RootReporting
 {
     private static TaskCompletionSource<int> cs;
 
-    // static async Task<int> Recursive(int n)
-    static async2 Task<int> Recursive(int n)
+
+    static async Task<int> Recursive1(int n)
     {
         Task<int> cTask = cs.Task;
 
@@ -32,7 +32,48 @@ public class Async2RootReporting
         if (n == 0)
             return await cTask;
 
-        var result = await Recursive(n - 1);
+        var result = await Recursive1(n - 1);
+
+        Assert.Equal(n, (int)o1);
+        //Assert.Equal(n, (int)o2);
+        //Assert.Equal(n, (int)o3);
+        //Assert.Equal(n, (int)o4);
+        //Assert.Equal(n, (int)o5);
+        //Assert.Equal(n, (int)o6);
+        //Assert.Equal(n, (int)o7);
+        //Assert.Equal(n, (int)o8);
+        //Assert.Equal(n, (int)o9);
+        //Assert.Equal(n, (int)o10);
+        //Assert.Equal(n, (int)o11);
+        //Assert.Equal(n, (int)o12);
+
+        return result;
+    }
+
+    static async2 Task<int> Recursive2(int n)
+    {
+        Task<int> cTask = cs.Task;
+
+        // make some garbage
+        object o1 = n;
+
+// THIS CAUSES CRASHES IN UNWINDER FLAVOR
+        //object o2 = n;
+        //object o3 = n;
+        //object o4 = n;
+        //object o5 = n;
+        //object o6 = n;
+        //object o7 = n;
+        //object o8 = n;
+        //object o9 = n;
+        //object o10 = n;
+        //object o11 = n;
+        //object o12 = n;
+
+        if (n == 0)
+            return await cTask;
+
+        var result = await Recursive2(n - 1);
 
         Assert.Equal(n, (int)o1);
         //Assert.Equal(n, (int)o2);
@@ -57,18 +98,21 @@ public class Async2RootReporting
         int stackDepth = 100;
         int numGCs = 100;
 
-        for (; ; )
+        Console.WriteLine("async-1 ===================== ");
+        var ticks = Environment.TickCount;
+
+        // run a few iterations for 5 seconds total
+        while (Environment.TickCount - ticks < 5000)
         {
             cs = new TaskCompletionSource<int>();
             Task[] tasks = new Task[numStacks];
             for (int i = 0; i < tasks.Length; i++)
             {
-                tasks[i] = Recursive(stackDepth);
+                tasks[i] = Recursive1(stackDepth);
             }
 
-            Stopwatch sw = Stopwatch.StartNew();
+            var ticksStart = Stopwatch.GetTimestamp();
 
-            // should not take too long since we are not alocating anything new.
             for (int i = 0; i < numGCs; i++)
             {
                 if (i % 10 == 0)
@@ -77,7 +121,36 @@ public class Async2RootReporting
                 GC.Collect(0);
             }
 
-            System.Console.WriteLine("Time: " + sw.ElapsedMilliseconds);
+            System.Console.WriteLine("Time per Gen0 GC (microseconds): " + (Stopwatch.GetTimestamp() - ticksStart) * 1000000 / numGCs / Stopwatch.Frequency);
+
+            cs.SetResult(100);
+            Task.WaitAll(tasks);
+        }
+
+        Console.WriteLine("async-2 ===================== ");
+        ticks = Environment.TickCount;
+
+        // run a few iterations for 5 seconds total
+        while (Environment.TickCount - ticks < 5000)
+        {
+            cs = new TaskCompletionSource<int>();
+            Task[] tasks = new Task[numStacks];
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = Recursive2(stackDepth);
+            }
+
+            var ticksStart = Stopwatch.GetTimestamp();
+
+            for (int i = 0; i < numGCs; i++)
+            {
+                if (i % 10 == 0)
+                    Console.Write('.');
+
+                GC.Collect(0);
+            }
+
+            System.Console.WriteLine("Time per Gen0 GC (microseconds): " + (Stopwatch.GetTimestamp() - ticksStart) * 1000000 / numGCs / Stopwatch.Frequency);
 
             cs.SetResult(100);
             Task.WaitAll(tasks);
