1243da298cf905f5e5ad7cea2ee52963917e8e9b
yowl00 <scott.waye@hubse.com>
2023-12-01 21:04:19 -0500

    add license header as now probably sufficiently different to the emscripten version

--- src/tests/nativeaot/SmokeTests/HelloWasm/shell.js
+++ src/tests/nativeaot/SmokeTests/HelloWasm/shell.js
@@ -1,7 +1,7 @@
-// No copyright header as this is largely a cut and paste from the standard
-// Emscripten produced shell
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
-// Allows the importing of symbols from a named module
+// Allows testing of Wasm imports from a named module
 
 var wasmBinaryFile = 'HelloWasm.wasm';
 
