f9e99519a8ee771abfd9e3f84ee584bbb4296bb2
Marek Fišera <mara@neptuo.com>
2023-12-13 13:27:01 +0100

    Fix typescript imports for NativeAOT

--- src/mono/wasm/runtime/run.ts
+++ src/mono/wasm/runtime/run.ts
@@ -1,7 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-import { ENVIRONMENT_IS_NODE, loaderHelpers, runtimeHelpers } from "./globals";
+import NativeAOT from "consts:nativeAOT";
+import { ENVIRONMENT_IS_NODE, Module, loaderHelpers, runtimeHelpers } from "./globals";
 import { mono_wasm_wait_for_debugger } from "./debug";
 import { mono_wasm_set_main_args } from "./startup";
 import cwraps from "./cwraps";
