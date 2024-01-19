452f25e06a961af14d5f90e0a9230311fdb6b6e4
SingleAccretion <AccretionMail@yandex.ru>
2024-01-19 18:42:11 +0300

    Add a license header

--- src/native/libs/System.Globalization.Native/icu_data_lib.c
+++ src/native/libs/System.Globalization.Native/icu_data_lib.c
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+//
+
 #define STR_IMPL(x) #x
 #define STR(x) STR_IMPL(x)
 
