497cbe183a9615cd95be480894504ff833d3da2e
Milos Kotlar <kotlarmilos@gmail.com>
2024-02-21 17:21:03 +0100

    Add src and tests directories for projects

---
+++ src/SwiftReflector/tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace SwiftReflector.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/SwiftRuntimeLibrary/tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace SwiftRuntimeLibrary.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
