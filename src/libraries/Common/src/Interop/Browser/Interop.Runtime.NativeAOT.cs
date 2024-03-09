44497c260ffb8b7bd9761f975c5f8a1839fcbffd
yowl00 <scott.waye@hubse.com>
2024-03-08 20:51:49 -0500

    some missed feedback
    add -Xlinker -export-if-defined=__main_argc_argv to CustomMain

--- src/libraries/Common/src/Interop/Browser/Interop.Runtime.NativeAOT.cs
+++ src/libraries/Common/src/Interop/Browser/Interop.Runtime.NativeAOT.cs
@@ -19,11 +19,11 @@ internal static partial class Interop
         [MethodImpl(MethodImplOptions.InternalCall)]
         public static extern void InvokeJSFunction(IntPtr bound_function_js_handle, nint data);
         [LibraryImport(JSLibrary, EntryPoint = "mono_wasm_invoke_js_import", StringMarshalling = StringMarshalling.Utf16)]
-        public static unsafe partial void InvokeJSImport(IntPtr fn_handle, void* data);
+        public static unsafe partial void InvokeJSImport(IntPtr fn_handle, nint data);
         [LibraryImport(JSLibrary, EntryPoint = "mono_wasm_bind_cs_function", StringMarshalling = StringMarshalling.Utf16)]
         public static unsafe partial void BindCSFunction(string fully_qualified_name, int fully_qualified_name_length, int signature_hash, void* signature, out int is_exception);
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern void ResolveOrRejectPromise(void* data);
+        public static extern void ResolveOrRejectPromise(nint data);
         [MethodImpl(MethodImplOptions.InternalCall)]
         public static extern IntPtr RegisterGCRoot(void* start, int bytesSize, IntPtr name);
         [MethodImpl(MethodImplOptions.InternalCall)]
