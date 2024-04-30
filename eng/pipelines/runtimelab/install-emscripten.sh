9fa9d2cffa815fa84261a9adb0ca4d1e007e927f
yowl <scott.waye@hubse.com>
2024-04-30 08:11:33 -0500

    Add install-emscripten.sh

---
+++ eng/pipelines/runtimelab/install-emscripten.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir "$1" 2>/dev/null
+cd "$1"
+
+git clone https://github.com/emscripten-core/emsdk.git
+
+cd emsdk
+# Checkout a known good version to avoid a random break when emscripten changes the top of tree.
+git checkout 37b85e9
+
+#powershell -NoProfile -NoLogo -ExecutionPolicy ByPass -command "& """%~dp0update-machine-certs.ps1""" %*"
+
+python emsdk.py install 3.1.47 || exit 1
+./emsdk activate 3.1.47 || exit 1
+
+# We key off of this variable in the common/build.ps1 script.
+echo Setting NATIVEAOT_CI_WASM_BUILD_EMSDK_PATH to $PWD
+echo ##vso[task.setvariable variable=NATIVEAOT_CI_WASM_BUILD_EMSDK_PATH]$PWD
