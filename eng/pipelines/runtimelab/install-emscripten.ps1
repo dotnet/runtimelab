ba2d7af4b4bd86d86600cda3860dd1578c54596c
yowl <scott.waye@hubse.com>
2024-05-07 15:25:19 -0500

    add ps1 file

---
+++ eng/pipelines/runtimelab/install-emscripten.ps1
@@ -0,0 +1,23 @@
+param(
+    [string]$InstallDir
+)
+
+New-Item -ItemType Directory -Force -ErrorAction SilentlyContinue -Name "$InstallDir"
+
+$ErrorActionPreference="Stop"
+
+Set-Location -Path "$InstallDir"
+
+git clone https://github.com/emscripten-core/emsdk.git
+
+Set-Location -Path emsdk
+
+# Checkout a specific commit to avoid unexpected issues
+git checkout 37b85e9
+
+python ./emsdk.py install 3.1.47
+
+./emsdk activate 3.1.47
+
+# Set a variable for later use (used in common/build.ps1)
+Write-Host "##vso[task.setvariable variable=NATIVEAOT_CI_WASM_BUILD_EMSDK_PATH]$PWD"
