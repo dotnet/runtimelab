dc36ffeb593893b5263c4a4da1d34ea9e0b38fa8
SingleAccretion <62474226+SingleAccretion@users.noreply.github.com>
2024-05-20 19:16:12 +0300

    Fix a typo
    Co-authored-by: yowl <scott.waye@hubse.com>

--- src/coreclr/nativeaot/Runtime/wasm/wasm.h
+++ src/coreclr/nativeaot/Runtime/wasm/wasm.h
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 // Define for a small subset of performance-critical FCalls that do not
-// have a shadow stack argument. Has not real functional importance and
+// have a shadow stack argument. Has no real functional importance and
 // serves as simply a marker for such FCalls.
 //
 #define FCIMPL_NO_SS(_rettype, _name, ...) extern "C" _rettype _name(__VA_ARGS__) {
