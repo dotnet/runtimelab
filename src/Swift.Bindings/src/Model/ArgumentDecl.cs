d4a8eedc2e91fc0da1d88a2f47ad250a58ae2ae5
Milos Kotlar <kotlarmilos@gmail.com>
2024-05-21 09:10:19 +0200

    Add license header to ArgumentDecl.cs

--- src/Swift.Bindings/src/Model/ArgumentDecl.cs
+++ src/Swift.Bindings/src/Model/ArgumentDecl.cs
@@ -1,22 +1,26 @@
-namespace BindingsGeneration;
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
-/// <summary>
-/// Represents an argument declaration.
-/// </summary>
-public record ArgumentDecl : BaseDecl {
+namespace BindingsGeneration
+{
+    /// <summary>
+    /// Represents an argument declaration.
+    /// </summary>
+    public record ArgumentDecl : BaseDecl {
 
-    /// <summary>
-    /// Type of the argument.
-    /// </summary>
-    public required TypeDecl TypeIdentifier { get; set; }
-    
-    /// <summary>
-    /// The private name of the argument.
-    /// </summary>
-    public required string PrivateName { get; set; }
+        /// <summary>
+        /// Type of the argument.
+        /// </summary>
+        public required TypeDecl TypeIdentifier { get; set; }
+        
+        /// <summary>
+        /// The private name of the argument.
+        /// </summary>
+        public required string PrivateName { get; set; }
 
-    /// <summary>
-    /// Indicates the inout annotation of the argument.
-    /// </summary>
-    public required bool IsInOut { get; set; }
+        /// <summary>
+        /// Indicates the inout annotation of the argument.
+        /// </summary>
+        public required bool IsInOut { get; set; }
+    }
 }
