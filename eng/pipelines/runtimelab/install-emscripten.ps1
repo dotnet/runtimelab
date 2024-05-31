ae7267d691ac1992a8430a617505c55d1a016ae9
yowl00 <scott.waye@hubse.com>
2024-05-31 09:44:38 +0100

    update llvm to 18.1.3 and emscripten to 3.1.54

--- eng/pipelines/runtimelab/install-emscripten.ps1
+++ eng/pipelines/runtimelab/install-emscripten.ps1
@@ -13,11 +13,11 @@ git clone https://github.com/emscripten-core/emsdk.git
 Set-Location -Path emsdk
 
 # Checkout a specific commit to avoid unexpected issues
-git checkout 37b85e9
+git checkout c18280c
 
-./emsdk install 3.1.47
+./emsdk install 3.1.54
 
-./emsdk activate 3.1.47
+./emsdk activate 3.1.54
 
 # Set a variable for later use (used in common/build.ps1)
 Write-Host "##vso[task.setvariable variable=NATIVEAOT_CI_WASM_BUILD_EMSDK_PATH]$PWD"
