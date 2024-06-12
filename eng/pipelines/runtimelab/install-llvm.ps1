4a0cb88d3ae60caadb800602cb32d12ba24c03a5
SingleAccretion <62474226+SingleAccretion@users.noreply.github.com>
2024-06-12 17:54:44 +0300

    A few tweaks/fixes to install-llvm.ps1 (#2611)
    Better account for the "upgrade local LLVM" scenario.

--- eng/pipelines/runtimelab/install-llvm.ps1
+++ eng/pipelines/runtimelab/install-llvm.ps1
@@ -1,45 +1,54 @@
 [CmdletBinding(PositionalBinding=$false)]
 param(
-    $InstallDir,
+    [string]$CloneDir = $null,
     [ValidateSet("Debug","Release","Checked")][string[]]$Configs = @("Debug","Release"),
     [switch]$CI,
     [switch]$NoClone,
     [switch]$NoBuild
 )
 
-
 $ErrorActionPreference="Stop"
-
-New-Item -ItemType Directory -Path $InstallDir -Force
-Set-Location -Path $InstallDir
 
 # Set IsWindows if the version of Powershell does not already have it.
 if (!(Test-Path variable:global:IsWindows)) 
 {
     $IsWindows = [Environment]::OSVersion.Platform -eq [PlatformID]::Win32NT
 }
-
 if (!(gcm git -ErrorAction SilentlyContinue))
 {
    Write-Error "Unable to find 'git' in PATH"
-   exit 1
 }
 if (!(gcm cmake -ErrorAction SilentlyContinue))
 {
    Write-Error "Unable to find 'cmake' in PATH"
-   exit 1
 }
 
-if (!$NoClone)
+if ($CloneDir -ne $null)
 {
-    $LlvmProjectTag = "llvmorg-18.1.3"
+    New-Item -ItemType Directory -Path $CloneDir -Force
+    Set-Location -Path $CloneDir
+}
+
+$LlvmProjectTag = "llvmorg-18.1.3"
+if ($NoClone)
+{
+    if (!(Test-Path llvm-project))
+    {
+        Write-Error "llvm-project repository not present in the current directory"
+    }
+
+    pushd llvm-project
+    git checkout $LlvmProjectTag
+    popd
+    if ($LastExitCode -ne 0)
+    {
+        Write-Error "git checkout failed (fetch the missing tag with 'git fetch --all --tags')"
+    }
+}
+else
+{
     $DepthOption = if ($CI) {"--depth","1"} else {}
     git clone https://github.com/llvm/llvm-project --branch $LlvmProjectTag $DepthOption
-}
-elseif (!(Test-Path llvm-project))
-{
-    Write-Error "llvm-project repository not present in the current directory"
-    exit 1
 }
 
 # There is no [C/c]hecked LLVM config, so change to Debug
@@ -84,12 +93,15 @@ foreach ($Config in $Configs | % { if ($_ -eq "Checked") { "Debug" } else { $_ }
 
     Write-Host "Invoking CMake configure: 'cmake $CmakeConfigureCommandLine'"
     cmake @CmakeConfigureCommandLine
+    if ($LastExitCode -ne 0)
+    {
+        Write-Error "CMake configure failed"
+    }
 
     if (!$NoBuild)
     {
         Write-Host "Invoking CMake --build"
-        cmake --build $BuildDirPath --config $LlvmConfig --target LLVMCore
-        cmake --build $BuildDirPath --config $LlvmConfig --target LLVMBitWriter
+        cmake --build $BuildDirPath --config $LlvmConfig --target LLVMCore LLVMBitWriter
     }
 
     $LlvmCmakeConfigPath = "$BuildDirPath/lib/cmake/llvm"
