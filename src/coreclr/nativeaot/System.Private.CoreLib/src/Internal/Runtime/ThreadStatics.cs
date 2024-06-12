e2a93321628133f22f454dc944ece487d648e214
yowl <scott.waye@hubse.com>
2024-06-12 20:29:40 +0100

    [NativeAOT-LLVM] Workaround to align thread statics to 8 byte boundary (#2609)
    * workaround to align thread statics to 8 byte boundary
    
    * add test for thread static alignment

--- src/coreclr/nativeaot/System.Private.CoreLib/src/Internal/Runtime/ThreadStatics.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/Internal/Runtime/ThreadStatics.cs
@@ -137,7 +137,13 @@ namespace Internal.Runtime
                 gcDesc = Internal.Runtime.Augments.RuntimeAugments.TypeLoaderCallbacks.GetThreadStaticGCDescForDynamicType(typeManager, typeTlsIndex);
             }
 
+#if TARGET_WASM
+            // TODO-LLVM: Remove when there is an upstream fix.  See https://github.com/dotnet/runtimelab/issues/2606 and https://github.com/dotnet/runtime/issues/103234
+            // This is a fix for aligning to 8, thread static fields that should be aligned 8, we just conservatively align everything to 8.
+            return InternalCalls.RhpNewFastAlign8((MethodTable*)gcDesc);
+#else
             return RuntimeImports.RhNewObject((MethodTable*)gcDesc);
+#endif
         }
     }
 }
