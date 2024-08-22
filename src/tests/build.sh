bc4e580f4313c82928a7ffdb5db0da27572bf4a0
yowl00 <scott.waye@hubse.com>
2024-08-22 08:50:00 -0500

    simplify script

--- src/tests/build.sh
+++ src/tests/build.sh
@@ -167,8 +167,8 @@ usage_list+=("-test:xxx - Only build the specified test project ^(relative or ab
 usage_list+=("-dir:xxx - Build all test projects in the given directory ^(relative or absolute directory under src\tests^).");
 usage_list+=("-tree:xxx - Build all test projects in the given subtree ^(relative or absolute directory under src\tests^).");
 usage_list+=("-log:xxx - Base file name to use for log files (used in lab pipelines that build tests in multiple steps to retain logs for each step).")
-usage_list+=("-target-os:xxx - Override the target OS.")
-usage_list+=("-arch:xxx - Override the target architecture.")
+usage_list+=("-broser - Target Wasm/browser.")
+usage_list+=("-wasi - Target Wasm/WASI.")
 usage_list+=("")
 usage_list+=("Any unrecognized arguments will be passed directly to MSBuild.")
 
@@ -320,28 +320,14 @@ handle_arguments_local() {
             fi
             ;;
 
-        target-os*|-target-os*)
-            local arg="$1"
-            local parts=(${arg//:/ })
-            if [[ ${#parts[@]} -eq 1 ]]; then
-                __TargetOS="$2"
-                __ShiftArgs=1
-            else
-                __TargetOS="${parts[1]}"
-
-            fi
+        browser|-browser)
+            __TargetOS=browser
+            __TargetArch=wasm
             ;;
 
-        arch*|-arch*)
-            local arg="$1"
-            local parts=(${arg//:/ })
-            if [[ ${#parts[@]} -eq 1 ]]; then
-                __TargetArch="$2"
-                __ShiftArgs=1
-            else
-                __TargetArch="${parts[1]}"
-
-            fi
+        wasi|-wasi)
+            __TargetOS=wasi
+            __TargetArch=wasm
             ;;
 
         *)
