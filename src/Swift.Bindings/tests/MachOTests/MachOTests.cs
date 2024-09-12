89a4840d5997eab8f50cfeb0f0cbac803ead05f4
Stephen Hawley <sthawl@microsoft.com>
2024-09-12 14:25:00 -0400

    More tests

--- src/Swift.Bindings/tests/MachOTests/MachOTests.cs
+++ src/Swift.Bindings/tests/MachOTests/MachOTests.cs
@@ -48,6 +48,21 @@ namespace BindingsGeneration.Tests
             }
 
             [Fact]
+            public static void IsCorrectMinOS()
+            {
+                var macho = MachO.Read(_dylibPath).FirstOrDefault();
+                Assert.NotNull(macho);
+                var minOS = macho.load_commands.OfType<MinCommand>().FirstOrDefault();
+                // this library is build without a minOS load command
+                Assert.Null(minOS);
+                var bv = macho.load_commands.OfType<BuildVersionCommand>().FirstOrDefault();
+                Assert.NotNull(bv);
+                Assert.Equal(new Version(11, 0, 0), bv.MinOS);
+                Assert.Equal(new Version(13, 3, 0), bv.Sdk);
+                Assert.Equal(MachO.Platform.MacOS, bv.Platform);
+            }
+
+            [Fact]
             public static void HasSymbols()
             {
                 var abis = MachO.GetArchitectures(_dylibPath);
@@ -56,6 +71,15 @@ namespace BindingsGeneration.Tests
                 // this depends on the contents of the library. If you changed
                 // the contents of FrozenStructs, then this will likely fail.
                 Assert.Equal(522, symbols.Count());
+            }
+
+            [Fact]
+            public static void NoPrivate()
+            {
+                var abis = MachO.GetArchitectures(_dylibPath);
+                var symbols = MachO.Read(_dylibPath).PublicSymbols(abis[0]);
+                var any = symbols.Any(sy => sy.IsPrivate);
+                Assert.False(any);
             }
         }
     }
