c3001b323bf7ca85328ccfb1dbaa85ef0cf1881c
vsadov <8218165+VSadov@users.noreply.github.com>
2024-12-12 10:30:50 -0800

    4 iterations / 100 msec delay

--- src/tests/async/pgo.cs
+++ src/tests/async/pgo.cs
@@ -22,12 +22,12 @@ public class Async2Pgo
         int[] arr = Enumerable.Range(0, 100_000).ToArray();
 
         int sum = 0;
-        for (int i = 0; i < 400; i++)
+        for (int i = 0; i < 4; i++)
         {
             for (int j = 0; j < 100; j++)
                 sum += await AggregateDelegateAsync(arr, new AggregateSum(), 0);
 
-            await Task.Delay(1);
+            await Task.Delay(100);
         }
 
         return sum;
