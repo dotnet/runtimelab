2a7b4444f8aff23ac440bf86a1f71a1ae0a50950
Jeremi Kurdek <jkurdek@gmail.com>
2024-12-19 21:06:52 +0100

    Initialized structs functional tests

---
+++ src/Swift.Bindings/tests/IntegrationTests/FunctionalTests/Structs/StructsTests.cs
@@ -0,0 +1,213 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Xunit;
+using Swift.StructsTests;
+
+namespace BindingsGeneration.FunctionalTests
+{
+    public class StructTests : IClassFixture<StructTests.TestFixture>
+    {
+        private readonly TestFixture _fixture;
+
+        public StructTests(TestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        public class TestFixture
+        {
+            static TestFixture()
+            {
+                InitializeResources();
+            }
+
+            private static void InitializeResources()
+            {
+                // Initialize
+            }
+        }
+
+        [Fact]
+        public void TestFrozenStructCreation()
+        {
+            IntPtr x = 1;
+            IntPtr y = 2;
+
+            var frozen = new FrozenStruct(x, y);
+            var gotX = frozen.getX();
+            var gotY = frozen.getY();
+
+            Assert.Equal(x, gotX);
+            Assert.Equal(y, gotY);
+        }
+
+        [Fact]
+        public void TestNonFrozenStructCreation()
+        {
+            IntPtr x = 1;
+            IntPtr y = 2;
+
+            var nonFrozen = new NonFrozenStruct(x, y);
+            var gotX = nonFrozen.getX();
+            var gotY = nonFrozen.getY();
+
+            Assert.Equal(x, gotX);
+            Assert.Equal(y, gotY);
+        }
+
+        // [Fact]
+        // public void TestNonFrozenStructWithNonFrozenMemberCreation()
+        // {
+        //     IntPtr frozenX = 1;
+        //     IntPtr frozenY = 2;
+        //     IntPtr nonFrozenX = 30;
+        //     IntPtr nonFrozenY = 40;
+
+        //     var frozen = new FrozenStruct(frozenX, frozenY);
+        //     var nonFrozen = new NonFrozenStruct(nonFrozenX, nonFrozenY);
+
+        //     var complexStruct = new NonFrozenStructWithNonFrozenMember(frozen, nonFrozen);
+        //     var gotF = complexStruct.getX();
+        //     var gotNF = complexStruct.getY();
+
+        //     Assert.Equal(frozenX, gotF.getX());
+        //     Assert.Equal(frozenY, gotF.getY());
+        //     Assert.Equal(nonFrozenX, gotNF.getX());
+        //     Assert.Equal(nonFrozenY, gotNF.getY());
+        // }
+
+        [Fact]
+        public void TestInstanceMethodOnFrozenStruct()
+        {
+            IntPtr x = 1;
+            IntPtr y = 2;
+
+            var frozen = new FrozenStruct(x, y);
+
+            var result = frozen.sum();
+
+            Assert.Equal(1 + 2, result);
+        }
+
+        [Fact]
+        public void TestInstanceMethodOnNonFrozenStruct()
+        {
+            IntPtr x = 1;
+            IntPtr y = 2;
+
+            var nonFrozen = new NonFrozenStruct(x, y);
+
+            var result = nonFrozen.sum();
+
+            Assert.Equal(1 + 2, result);
+        }
+
+        [Fact]
+        public void TestInstanceMethodOnNonFrozenStructWithNonFrozenMember()
+        {
+            IntPtr frozenX = 1;
+            IntPtr frozenY = 2;
+            IntPtr nonFrozenX = 30;
+            IntPtr nonFrozenY = 40;
+
+            var frozen = new FrozenStruct(frozenX, frozenY);
+            var nonFrozen = new NonFrozenStruct(nonFrozenX, nonFrozenY);
+            var complexStruct = new NonFrozenStructWithNonFrozenMember(frozen, nonFrozen);
+
+            var result = complexStruct.sum();
+
+            Assert.Equal(1 + 2 + 30 + 40, result);
+        }
+
+        [Fact]
+        public void TestModuleFuncWithFrozenAndNonFrozenParameters()
+        {
+            IntPtr frozenX = 1;
+            IntPtr frozenY = 2;
+            IntPtr nonFrozenX = 30;
+            IntPtr nonFrozenY = 40;
+
+            var frozen = new FrozenStruct(frozenX, frozenY);
+            var nonFrozen = new NonFrozenStruct(nonFrozenX, nonFrozenY);
+
+            var result = StructsTests.sumFrozenAndNonFrozen(frozen, nonFrozen);
+
+            Assert.Equal(1 + 2 + 30 + 40, result);
+        }
+
+        [Fact]
+        public void TestModuleFuncReturningFrozenStruct()
+        {
+            IntPtr x = 1;
+            IntPtr y = 2;
+
+            var result = StructsTests.createFrozenStruct(x, y);
+
+            Assert.Equal(x, result.getX());
+            Assert.Equal(y, result.getY());
+        }
+
+        // [Fact]
+        // public void TestModuleFuncReturningNonFrozenStruct()
+        // {
+        //     IntPtr x = 1;
+        //     IntPtr y = 2;
+
+        //     var result = StructsTests.createNonFrozenStruct(x, y);
+
+        //     Assert.Equal(x, result.getX());
+        //     Assert.Equal(y, result.getY());
+        // }
+
+        [Fact]
+        public void TestInstanceMethodReturningFrozenStruct()
+        {
+            IntPtr x = 1;
+            IntPtr y = 2;
+            var structBuilder = new StructBuilder(x, y);
+
+            var result = structBuilder.createFrozenStruct();
+
+            Assert.Equal(x, result.getX());
+            Assert.Equal(y, result.getY());
+        }
+
+        // [Fact]
+        // public void TestInstanceMethodReturningNonFrozenStruct()
+        // {
+        //     IntPtr x = 1;
+        //     IntPtr y = 2;
+        //     var structBuilder = new StructBuilder(x, y);
+
+        //     var result = structBuilder.createNonFrozenStruct();
+
+        //     Assert.Equal(x, result.getX());
+        //     Assert.Equal(y, result.getY());
+        // }
+
+        [Fact]
+        public void TestStaticMethodReturningFrozenStruct()
+        {
+            IntPtr x = 1;
+            IntPtr y = 2;
+
+            var result = StructBuilder.createFrozenStruct(x, y);
+
+            Assert.Equal(x, result.getX());
+            Assert.Equal(y, result.getY());
+        }
+
+        // [Fact]
+        // public void TestStaticMethodReturningNonFrozenStruct()
+        // {
+        //     IntPtr x = 1;
+        //     IntPtr y = 2;
+
+        //     var result = StructBuilder.createNonFrozenStruct(x, y);
+
+        //     Assert.Equal(x, result.getX());
+        //     Assert.Equal(y, result.getY());
+        // }
+    }
+}
---
+++ src/Swift.Bindings/tests/IntegrationTests/FunctionalTests/Structs/StructsTests.swift
@@ -0,0 +1,158 @@
+import Foundation
+
+@frozen
+public struct FrozenStruct
+{
+    public var x: Int
+    public var y: Int
+
+    public init(x: Int, y: Int)
+    {
+        self.x = x
+        self.y = y
+    }
+
+    public func getX() -> Int
+    {
+        return x
+    }
+
+    public func getY() -> Int
+    {
+        return y
+    }
+
+    public func sum() -> Int
+    {
+        return x + y
+    }
+}
+
+public struct NonFrozenStruct
+{
+    public var x: Int
+    public var y: Int
+
+    public init(x: Int, y: Int)
+    {
+        self.x = x
+        self.y = y
+    }
+
+    public func getX() -> Int
+    {
+        return x
+    }
+
+    public func getY() -> Int
+    {
+        return y
+    }
+
+    public func sum() -> Int
+    {
+        return x + y
+    }
+}
+
+@frozen 
+public struct FrozenStructWithNonFrozenMember
+{
+    public var x: FrozenStruct
+    public var y: NonFrozenStruct
+
+    public init(x: FrozenStruct, y: NonFrozenStruct)
+    {
+        self.x = x
+        self.y = y
+    }
+
+    public func getX() -> FrozenStruct
+    {
+        return x
+    }
+
+    public func getY() -> NonFrozenStruct
+    {
+        return y
+    }
+
+    public func sum(a: NonFrozenStruct, b: FrozenStruct) -> Int
+    {
+        return x.sum() + y.sum() + a.sum() + b.sum()
+    }
+}
+
+public struct NonFrozenStructWithNonFrozenMember
+{
+    public var x: FrozenStruct
+    public var y: NonFrozenStruct
+
+    public init(x: FrozenStruct, y: NonFrozenStruct)
+    {
+        self.x = x
+        self.y = y
+    }
+
+    public func getX() -> FrozenStruct
+    {
+        return x
+    }
+
+    public func getY() -> NonFrozenStruct
+    {
+        return y
+    }
+
+    public func sum() -> Int
+    {
+        return x.sum() + y.sum()
+    }
+}
+
+public struct StructBuilder
+{
+    public let x: Int
+    public let y: Int
+
+    public init(x: Int, y: Int)
+    {
+        self.x = x
+        self.y = y
+    }
+
+    public func createFrozenStruct() -> FrozenStruct
+    {
+        return FrozenStruct(x: x, y: y)
+    }
+
+    public func createNonFrozenStruct() -> NonFrozenStruct
+    {
+        return NonFrozenStruct(x: x, y: y)
+    }
+
+    public static func createFrozenStruct(x: Int, y: Int) -> FrozenStruct
+    {
+        return FrozenStruct(x: x, y: y)
+    }
+
+    public static func createNonFrozenStruct(x: Int, y: Int) -> NonFrozenStruct
+    {
+        return NonFrozenStruct(x: x, y: y)
+    }
+}
+
+public func sumFrozenAndNonFrozen(a: FrozenStruct, b: NonFrozenStruct) -> Int
+{
+    return a.sum() + b.sum()
+}
+
+public func createFrozenStruct(a: Int, b: Int) -> FrozenStruct
+{
+    return FrozenStruct(x: a, y: b)
+}
+
+public func createNonFrozenStruct(a: Int, b: Int) -> NonFrozenStruct
+{
+    return NonFrozenStruct(x: a, y: b)
+}
