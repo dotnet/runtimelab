c1e8cf542e3f12e3398877833192ef497e06f49f
Matous Kozak <matouskozak@seznam.cz>
2025-03-13 16:17:10 +0000

    fix trimming of '_' from Symbol Name

--- src/Swift.Bindings/src/Demangler/DemanglingResults.cs
+++ src/Swift.Bindings/src/Demangler/DemanglingResults.cs
@@ -120,20 +120,9 @@ public class DemanglingResults
 
         var demangler = new Swift5Demangler();
 
-        // Remove '_' prefix from Swift symbols if present
-        foreach (var export in tbdFile.Exports)
-        {
-            foreach (var symbol in export.SwiftSymbols)
-            {
-                if (symbol.Name.StartsWith('_'))
-                {
-                    symbol.Name = symbol.Name[1..];
-                }
-            }
-        }
-
         // Run demangler for each export and aggregate results
-        var allReductions = tbdFile.Exports.SelectMany(export => export.SwiftSymbols.Select(sym => demangler.Run(sym.Name))).ToArray();
+        var allReductions = tbdFile.Exports.SelectMany(export => export.SwiftSymbols.Select(sym => 
+            demangler.Run(sym.Name.StartsWith('_') ? sym.Name[1..] : sym.Name))).ToArray();
         return new DemanglingResults(allReductions);
     }
 
