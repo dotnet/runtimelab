4121ce3a595111313c3701ae6d40c32d2a522bd5
Jeremi Kurdek <jkurdek@gmail.com>
2025-03-14 09:42:43 +0100

    Fix using statements order

--- src/Swift.Runtime/src/Swift/SwiftEquatable.cs
+++ src/Swift.Runtime/src/Swift/SwiftEquatable.cs
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Swift;
 using Swift.Runtime.InteropServices;
 
