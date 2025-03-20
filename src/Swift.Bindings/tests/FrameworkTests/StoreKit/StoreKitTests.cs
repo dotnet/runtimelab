8f371e47b5711a63a89238a5a6ec16ea7977eb7b
Milos Kotlar <kotlarmilos@gmail.com>
2025-03-20 14:14:10 +0100

    Run StoreKit tests

--- src/Swift.Bindings/tests/FrameworkTests/StoreKit/StoreKitTests.cs
+++ src/Swift.Bindings/tests/FrameworkTests/StoreKit/StoreKitTests.cs
@@ -16,19 +16,109 @@ public static class MauiProgram
 			_output = output;
 		}
 
-        // Tracking issue: https://github.com/dotnet/runtimelab/issues/2850
 		[Fact]
-		public async Task Test()
+		public async Task TestProductProducts()
 		{
             var productIdentifiers = new SwiftArray<SwiftString>();
-            productIdentifiers.Append(new SwiftString("com.example.product1"));
-            productIdentifiers.Append(new SwiftString("com.example.product2"));
-            productIdentifiers.Append(new SwiftString("com.example.product3"));
+            productIdentifiers.Append(new SwiftString("p1"));
+            productIdentifiers.Append(new SwiftString("p2"));
 			var productsTask = Product.products<SwiftArray<SwiftString>>(productIdentifiers);
             SwiftArray<Product> products = await productsTask;
             Assert.NotNull(products);
-            // TODO: Add .storekit config
+            // StoreKit configuration is not set
             Assert.Equal(0, products.Count);
 		}
+
+        [Fact]
+        public void TestProductTypes()
+        {
+            var productTypes = new SwiftArray<Product.ProductType>();
+            productTypes.Append(Product.ProductType.consumable);
+            productTypes.Append(Product.ProductType.nonConsumable);
+            productTypes.Append(Product.ProductType.nonRenewable);
+            productTypes.Append(Product.ProductType.autoRenewable);
+
+            Assert.True (productTypes[0] == Product.ProductType.consumable);
+            Assert.True (productTypes[1] == Product.ProductType.nonConsumable);
+            Assert.True (productTypes[2] == Product.ProductType.nonRenewable);
+            Assert.True (productTypes[3] == Product.ProductType.autoRenewable);
+        }
+
+        [Fact]
+        public void TestAppStoreProperties()
+        {
+            bool canMakePayments = AppStore.canMakePayments;
+            Assert.True(canMakePayments);
+
+            SwiftString production = AppStore.Environment.production.rawValue;
+            SwiftString sandbox = AppStore.Environment.sandbox.rawValue;
+            SwiftString xcode = AppStore.Environment.xcode.rawValue;
+            Assert.NotNull(production.ToString());
+            Assert.NotNull(sandbox.ToString());
+            Assert.NotNull(xcode.ToString());
+        }
+
+        [Fact]
+        public void TestPurchaseOptionCustomMethods()
+        {
+            var opt1 = Product.PurchaseOption.custom(new SwiftString("key1"), new SwiftString("value1"));
+            Assert.NotNull(opt1);
+            var opt2 = Product.PurchaseOption.custom(new SwiftString("key2"), 9.99);
+            Assert.NotNull(opt2);
+        }
+
+        [Fact(Skip = "https://github.com/dotnet/runtimelab/issues/2850")]
+        public void TestPurchaseOptionCustomMethodsBool()
+        {
+            var opt3 = Product.PurchaseOption.custom(new SwiftString("key3"), true);
+            Assert.NotNull(opt3);
+        }
+
+        [Fact]
+        public void TestSubscriptionPeriodStaticProperties()
+        {
+            var weekly = Product.SubscriptionPeriod.weekly;
+            var monthly = Product.SubscriptionPeriod.monthly;
+            var yearly = Product.SubscriptionPeriod.yearly;
+            var everyThreeDays = Product.SubscriptionPeriod.everyThreeDays;
+            var everyTwoWeeks = Product.SubscriptionPeriod.everyTwoWeeks;
+            var everyTwoMonths = Product.SubscriptionPeriod.everyTwoMonths;
+            var everyThreeMonths = Product.SubscriptionPeriod.everyThreeMonths;
+            var everySixMonths = Product.SubscriptionPeriod.everySixMonths;
+
+            Assert.NotNull(weekly);
+            Assert.NotNull(monthly);
+            Assert.NotNull(yearly);
+            Assert.NotNull(everyThreeDays);
+            Assert.NotNull(everyTwoWeeks);
+            Assert.NotNull(everyTwoMonths);
+            Assert.NotNull(everyThreeMonths);
+            Assert.NotNull(everySixMonths);
+        }
+
+        [Fact]
+        public void TestSubscriptionOfferNestedTypes()
+        {
+            var intro = Product.SubscriptionOffer.OfferType.introductory;
+            var promo = Product.SubscriptionOffer.OfferType.promotional;
+            var winBack = Product.SubscriptionOffer.OfferType.winBack;
+            Assert.NotNull(intro);
+            Assert.NotNull(promo);
+            Assert.NotNull(winBack);
+
+            var payAsYouGo = Product.SubscriptionOffer.PaymentMode.payAsYouGo;
+            var payUpFront = Product.SubscriptionOffer.PaymentMode.payUpFront;
+            var freeTrial = Product.SubscriptionOffer.PaymentMode.freeTrial;
+            Assert.NotNull(payAsYouGo);
+            Assert.NotNull(payUpFront);
+            Assert.NotNull(freeTrial);
+        }
+
+        [Fact(Skip = "https://github.com/dotnet/runtimelab/issues/2850")]
+        public void TestExternalPurchaseProperties()
+        {
+            bool canPresent = ExternalPurchase.canPresent;
+            Assert.False(canPresent);
+        }
 	}
 }
