9be9a4fa86777c22c50e95a06495638ef40bca5b
Milos Kotlar <kotlarmilos@gmail.com>
2025-03-24 19:05:51 +0100

    Add .tbd file to the sample script

--- src/samples/HelloWorld/build.sh
+++ src/samples/HelloWorld/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 xcrun swiftc -emit-module -emit-library -enable-library-evolution -emit-module-interface HelloLibrary.swift  -o libHelloLibrary.dylib
+xcrun tapi stubify --filetype=tbd-v4  libHelloLibrary.dylib -o libHelloLibrary.tbd
 
 dotnet restore ../../Swift.Bindings/src/Swift.Bindings.csproj
 dotnet build ../../Swift.Bindings/src/Swift.Bindings.csproj
 
 if [ $? -eq 0 ]; then
     echo "Build successful. Running the BindingsTool..."
-    dotnet ../../../artifacts/bin/Swift.Bindings/Debug/net9.0/Swift.Bindings.dll -a "./HelloLibrary.abi.json" -d "./libHelloLibrary.dylib" -o "./"
+    dotnet ../../../artifacts/bin/Swift.Bindings/Debug/net9.0/Swift.Bindings.dll -a "./HelloLibrary.abi.json" -d "./libHelloLibrary.dylib" -t "./libHelloLibrary.tbd" -o "./"
     dotnet run
 else
     echo "Build failed. Exiting..."
