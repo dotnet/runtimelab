6253f18d9ac15d526a72fc5f259b588e041b2cc2
SingleAccretion <AccretionMail@yandex.ru>
2025-10-28 23:50:03 +0300

    Add support for `<NativeLib>Static</NativeLib>`
    Refactor the targets and sink more WASM specifics into the WASM-specific targets.

---
+++ src/tests/nativeaot/SmokeTests/HelloWasm/WasmStaticLib.cs
@@ -0,0 +1,101 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class StaticLibrary
+{
+    static int s_primitiveInt = 10;
+
+    [UnmanagedCallersOnly(EntryPoint = "ReturnsPrimitiveInt")]
+    public static int ReturnsPrimitiveInt()
+    {
+        return s_primitiveInt;
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "ReturnsPrimitiveBool")]
+    public static bool ReturnsPrimitiveBool()
+    {
+        return true;
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "ReturnsPrimitiveChar")]
+    public static char ReturnsPrimitiveChar()
+    {
+        return 'a';
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "EnsureManagedClassLoaders")]
+    public static void EnsureManagedClassLoaders()
+    {
+        Random random = new Random();
+        random.Next();
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "CheckSimpleExceptionHandling")]
+    public static int CheckSimpleExceptionHandling()
+    {
+        int result = 10;
+
+        try
+        {
+            Console.WriteLine("Throwing exception");
+            throw new Exception();
+        }
+        catch when (result == 10)
+        {
+            result += 20;
+        }
+        finally
+        {
+            result += 70;
+        }
+
+        return result;
+    }
+
+    private static bool s_collected;
+
+    class ClassWithFinalizer
+    {
+        ~ClassWithFinalizer() { s_collected = true; }
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static void MakeGarbage()
+    {
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+
+        object[] arr = new object[1024 * 1024];
+        for (int i = 0; i < arr.Length; i++)
+            arr[i] = new object();
+
+        new ClassWithFinalizer();
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "CheckSimpleGCCollect")]
+    public static int CheckSimpleGCCollect()
+    {
+        string myString = string.Format("Hello {0}", "world");
+
+        MakeGarbage();
+
+        Console.WriteLine("Triggering GC");
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+
+        return s_collected ? (myString == "Hello world" ? 100 : 1) : 2;
+    }
+}
