0af42e640c4964ebfcc3ca7a9bc2a2f6d83011cc
@yowl <scott@waye.co.uk>
2026-01-06 18:17:57 -0500

    remove debug

--- eng/native/gen-buildsys.sh
+++ eng/native/gen-buildsys.sh
@@ -117,8 +117,6 @@ if [[ "$host_arch" == "wasm" ]]; then
                 exit 1
             fi
         fi
-        echo "setting DCMAKE_TOOLCHAIN_FILE to ${WASI_SDK_PATH}/share/cmake/wasi-sdk-p2.cmake"
-        cat ${WASI_SDK_PATH}/share/cmake/wasi-sdk-p2.cmake
         cmake_extra_defines_wasm=("-DCLR_CMAKE_TARGET_OS=wasi" "-DCMAKE_TOOLCHAIN_FILE=${WASI_SDK_PATH}/share/cmake/wasi-sdk-p2.cmake" "-DCMAKE_CROSSCOMPILING_EMULATOR=node --experimental-wasm-bigint --experimental-wasi-unstable-preview1")
     else
         echo "target_os was not specified"
