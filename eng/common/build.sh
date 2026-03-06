cc07a93f9c779afbe614c671d84c78f3237e6428
Eric StJohn <ericstj@microsoft.com>
2026-03-06 11:05:39 -0800

    Update to 10.0 arcade and fix issues.

--- eng/common/build.sh
+++ eng/common/build.sh
@@ -19,6 +19,12 @@ usage()
   echo "Actions:"
   echo "  --restore                  Restore dependencies (short: -r)"
   echo "  --build                    Build solution (short: -b)"
+  echo "  --sourceBuild              Source-build the solution (short: -sb)"
+  echo "                             Will additionally trigger the following actions: --restore, --build, --pack"
+  echo "                             If --configuration is not set explicitly, will also set it to 'Release'"
+  echo "  --productBuild             Build the solution in the way it will be built in the full .NET product (VMR) build (short: -pb)"
+  echo "                             Will additionally trigger the following actions: --restore, --build, --pack"
+  echo "                             If --configuration is not set explicitly, will also set it to 'Release'"
   echo "  --rebuild                  Rebuild solution"
   echo "  --test                     Run all unit tests in the solution (short: -t)"
   echo "  --integrationTest          Run all integration tests in the solution"
@@ -36,6 +42,8 @@ usage()
   echo "  --prepareMachine         Prepare machine for CI run, clean up processes after build"
   echo "  --nodeReuse <value>      Sets nodereuse msbuild parameter ('true' or 'false')"
   echo "  --warnAsError <value>    Sets warnaserror msbuild parameter ('true' or 'false')"
+  echo "  --buildCheck <value>     Sets /check msbuild parameter"
+  echo "  --fromVMR                Set when building from within the VMR"
   echo ""
   echo "Command line arguments not listed above are passed thru to msbuild."
   echo "Arguments can also be passed in with a single hyphen."
@@ -55,6 +63,9 @@ scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
 
 restore=false
 build=false
+source_build=false
+product_build=false
+from_vmr=false
 rebuild=false
 test=false
 integration_test=false
@@ -68,18 +79,19 @@ clean=false
 
 warn_as_error=true
 node_reuse=true
+build_check=false
 binary_log=false
 exclude_ci_binary_log=false
 pipelines_log=false
 
 projects=''
-configuration='Debug'
+configuration=''
 prepare_machine=false
 verbosity='minimal'
 runtime_source_feed=''
 runtime_source_feed_key=''
 
-properties=''
+properties=()
 while [[ $# > 0 ]]; do
   opt="$(echo "${1/#--/-}" | tr "[:upper:]" "[:lower:]")"
   case "$opt" in
@@ -101,7 +113,7 @@ while [[ $# > 0 ]]; do
     -binarylog|-bl)
       binary_log=true
       ;;
-    -excludeCIBinarylog|-nobl)
+    -excludecibinarylog|-nobl)
       exclude_ci_binary_log=true
       ;;
     -pipelineslog|-pl)
@@ -118,6 +130,22 @@ while [[ $# > 0 ]]; do
       ;;
     -pack)
       pack=true
+      ;;
+    -sourcebuild|-source-build|-sb)
+      build=true
+      source_build=true
+      product_build=true
+      restore=true
+      pack=true
+      ;;
+    -productbuild|-product-build|-pb)
+      build=true
+      product_build=true
+      restore=true
+      pack=true
+      ;;
+    -fromvmr|-from-vmr)
+      from_vmr=true
       ;;
     -test|-t)
       test=true
@@ -152,6 +180,9 @@ while [[ $# > 0 ]]; do
       node_reuse=$2
       shift
       ;;
+    -buildcheck)
+      build_check=true
+      ;;
     -runtimesourcefeed)
       runtime_source_feed=$2
       shift
@@ -161,12 +192,16 @@ while [[ $# > 0 ]]; do
       shift
       ;;
     *)
-      properties="$properties $1"
+      properties+=("$1")
       ;;
   esac
 
   shift
 done
+
+if [[ -z "$configuration" ]]; then
+  if [[ "$source_build" = true ]]; then configuration="Release"; else configuration="Debug"; fi
+fi
 
 if [[ "$ci" == true ]]; then
   pipelines_log=true
@@ -191,7 +226,7 @@ function Build {
   InitializeCustomToolset
 
   if [[ ! -z "$projects" ]]; then
-    properties="$properties /p:Projects=$projects"
+    properties+=("/p:Projects=$projects")
   fi
 
   local bl=""
@@ -199,12 +234,21 @@ function Build {
     bl="/bl:\"$log_dir/Build.binlog\""
   fi
 
+  local check=""
+  if [[ "$build_check" == true ]]; then
+    check="/check"
+  fi
+
   MSBuild $_InitializeToolset \
     $bl \
+    $check \
     /p:Configuration=$configuration \
     /p:RepoRoot="$repo_root" \
     /p:Restore=$restore \
     /p:Build=$build \
+    /p:DotNetBuild=$product_build \
+    /p:DotNetBuildSourceOnly=$source_build \
+    /p:DotNetBuildFromVMR=$from_vmr \
     /p:Rebuild=$rebuild \
     /p:Test=$test \
     /p:Pack=$pack \
@@ -212,7 +256,8 @@ function Build {
     /p:PerformanceTest=$performance_test \
     /p:Sign=$sign \
     /p:Publish=$publish \
-    $properties
+    /p:RestoreStaticGraphEnableBinaryLogger=$binary_log \
+    ${properties[@]+"${properties[@]}"}
 
   ExitWithExitCode 0
 }
